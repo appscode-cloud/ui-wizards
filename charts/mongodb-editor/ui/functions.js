90a939fa6a01a7353d4dda635b5e46bc80e81be7
M. A. Muhaimin Sakib <sakib@appscode.com>
2021-01-21 11:12:22 +0600

    Add main form ui, language and functions file
    Signed-off-by: M. A. Muhaimin Sakib <sakib@appscode.com>

---
+++ charts/mongodb-editor/ui/functions.js
@@ -0,0 +1,150 @@
+export function isEqualToModelPathValue(
+  { model, getValue, watchDependency },
+  value,
+  modelPath
+) {
+  const modelPathValue = getValue(model, modelPath);
+  const modelName = modelPath.split("/").pop();
+  watchDependency(modelName, "model#" + modelPath);
+  return modelPathValue === value;
+}
+
+export function showAuthPasswordField({ model, getValue, watchDependency }) {
+  const modelPathValue = getValue(model, "/spec/authSecret/create");
+  watchDependency(modelPathValue, "model#/spec/authSecret/create");
+  return modelPathValue;
+}
+
+export function showAuthSecretField({ model, getValue, watchDependency }) {
+  return !this.showAuthPasswordField({ model, getValue, watchDependency });
+}
+
+export async function getResources(
+  { axios, storeGet },
+  group,
+  version,
+  resource
+) {
+  const owner = storeGet("/user/username");
+  const cluster = storeGet("/clusterInfo/name");
+
+  const resp = await axios.get(
+    `/clusters/${owner}/${cluster}/proxy/${group}/${version}/${resource}`,
+    {
+      params: { filter: { items: { metadata: { name: null } } } },
+    }
+  );
+
+  const resources = (resp && resp.data && resp.data.items) || [];
+
+  resources.map((item) => {
+    const name = (item.metadata && item.metadata.name) || "";
+    item.text = name;
+    item.value = name;
+    return true;
+  });
+  return resources;
+}
+
+export async function getMongoDbVersions(
+  { axios, storeGet },
+  group,
+  version,
+  resource
+) {
+  const owner = storeGet("/user/username");
+  const cluster = storeGet("/clusterInfo/name");
+
+  const queryParams = {
+    filter: { items: { metadata: { name: null }, spec: { version: null } } },
+  };
+
+  const resp = await axios.get(
+    `/clusters/${owner}/${cluster}/proxy/${group}/${version}/${resource}`,
+    {
+      params: queryParams,
+    }
+  );
+
+  const resources = (resp && resp.data && resp.data.items) || [];
+
+  resources.map((item) => {
+    const name = (item.metadata && item.metadata.name) || "";
+    const specVersion = (item.spec && item.spec.version) || "";
+    item.text = `${name} (${specVersion})`;
+    item.value = name;
+    return true;
+  });
+  return resources;
+}
+
+export async function getSecrets({
+  storeGet,
+  axios,
+  model,
+  getValue,
+  watchDependency,
+}) {
+  const owner = storeGet("/user/username");
+  const cluster = storeGet("/clusterInfo/name");
+  const namespace = getValue(model, "/namespace");
+  watchDependency("namespace", "model#/namespace");
+
+  const resp = await axios.get(
+    `/clusters/${owner}/${cluster}/proxy/core/v1/namespaces/${namespace}/secrets`,
+    {
+      params: {
+        filter: { items: { metadata: { name: null }, type: null } },
+      },
+    }
+  );
+
+  const secrets = (resp && resp.data && resp.data.items) || [];
+
+  const filteredSecrets = secrets.filter((item) => {
+    const validType = ["kubernetes.io/service-account-token", "Opaque"];
+    return validType.includes(item.type);
+  });
+
+  filteredSecrets.map((item) => {
+    const name = (item.metadata && item.metadata.name) || "";
+    item.text = name;
+    item.value = name;
+    return true;
+  });
+  return filteredSecrets;
+}
+
+export async function hasExistingSecret({
+  storeGet,
+  axios,
+  model,
+  getValue,
+  watchDependency,
+}) {
+  const resp = await this.getSecrets({
+    storeGet,
+    axios,
+    model,
+    getValue,
+    watchDependency,
+  });
+  return !!(resp && resp.length);
+}
+
+export async function hasNoExistingSecret({
+  storeGet,
+  axios,
+  model,
+  getValue,
+  watchDependency,
+}) {
+  const resp = await this.hasExistingSecret({
+    storeGet,
+    axios,
+    model,
+    getValue,
+    watchDependency,
+  });
+  return !resp;
+}
