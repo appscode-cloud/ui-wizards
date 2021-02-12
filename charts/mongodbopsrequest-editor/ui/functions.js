8533baf162ca6a152c4ed3a817e94ae15ce6f857
M. A. Muhaimin Sakib <sakib@appscode.com>
2021-02-12 16:13:50 +0600

    Add yamls for mongo db op request create
    Signed-off-by: M. A. Muhaimin Sakib <sakib@appscode.com>

---
+++ charts/mongodbopsrequest-editor/ui/functions.js
@@ -0,0 +1,103 @@
+async function getNamespaces({ axios, storeGet }) {
+  const owner = storeGet("/user/username");
+  const cluster = storeGet("/clusterInfo/name");
+
+  const resp = await axios.get(
+    `/clusters/${owner}/${cluster}/proxy/core/v1/namespaces`,
+    {
+      params: { filter: { items: { metadata: { name: null } } } },
+    }
+  );
+
+  const resources = (resp && resp.data && resp.data.items) || [];
+
+  return resources.map((item) => {
+    const name = (item.metadata && item.metadata.name) || "";
+    return {
+      text: name,
+      value: name,
+    };
+  });
+}
+
+async function getMongoDbs({
+  axios,
+  storeGet,
+  model,
+  getValue,
+  watchDependency,
+}) {
+  const owner = storeGet("/user/username");
+  const cluster = storeGet("/clusterInfo/name");
+
+  const namespace = getValue(model, "/metadata/namespace");
+  watchDependency("model#/metadata/namespace");
+
+  const resp = await axios.get(
+    `/clusters/${owner}/${cluster}/proxy/kubedb.com/v1alpha2/namespaces/${namespace}/mongodbs`,
+    {
+      params: { filter: { items: { metadata: { name: null } } } },
+    }
+  );
+
+  const resources = (resp && resp.data && resp.data.items) || [];
+
+  return resources.map((item) => {
+    const name = (item.metadata && item.metadata.name) || "";
+    return {
+      text: name,
+      value: name,
+    };
+  });
+}
+
+async function getMongoDbVersions({ axios, storeGet }) {
+  const owner = storeGet("/user/username");
+  const cluster = storeGet("/clusterInfo/name");
+
+  const queryParams = {
+    filter: {
+      items: {
+        metadata: { name: null },
+        spec: { version: null, deprecated: null },
+      },
+    },
+  };
+
+  const resp = await axios.get(
+    `/clusters/${owner}/${cluster}/proxy/catalog.kubedb.com/v1alpha1/mongodbversions`,
+    {
+      params: queryParams,
+    }
+  );
+
+  const resources = (resp && resp.data && resp.data.items) || [];
+
+  // keep only non deprecated versions
+  const filteredMongoDbVersions = resources.filter(
+    (item) => item.spec && !item.spec.deprecated
+  );
+
+  return filteredMongoDbVersions.map((item) => {
+    const name = (item.metadata && item.metadata.name) || "";
+    const specVersion = (item.spec && item.spec.version) || "";
+    return {
+      text: `${name} (${specVersion})`,
+      value: name,
+    };
+  });
+}
+
+function ifRequestTypeEqualsTo({ model, getValue, watchDependency }, type) {
+  const selectedType = getValue(model, "/spec/type");
+  watchDependency("model#/spec/type");
+
+  return selectedType === type;
+}
+
+return {
+  getNamespaces,
+  getMongoDbs,
+  getMongoDbVersions,
+  ifRequestTypeEqualsTo,
+};
