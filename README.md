5dcb4eae641f219af604179d31178e01e98a4469
Tamal Saha <tamal@appscode.com>
2021-03-23 18:23:59 -0700

    Add github back dev helm chart repository
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- README.md
+++ README.md
@@ -9,15 +9,9 @@ helm repo add bytebuilders-ui https://bundles.byte.builders/ui/
 helm repo update
 ```
 
-### Generate mongodb-editor chart
+## Configure Development Helm Chart Repository
 
-```
-$ cd /home/tamal/go/src/kubepack.dev/lib-app
-$ go run cmd/fuse-chart/*.go \
-  --sample-name=mongodb \
-  --sample-dir=/home/tamal/go/src/github.com/appscode/kubedb-samples/mongodb/standalone/prometheus.io/backupconfiguration/stash/tls/custom-auth/standalone \
-  --chart-dir=/home/tamal/go/src/go.bytebuilders.dev/ui-wizards/charts \
-  --resource.group=kubedb.com \
-  --resource.version=v1alpha2 \
-  --resource.name=mongodbs
+```console
+helm repo add bytebuilders-ui-dev https://raw.githubusercontent.com/bytebuilders/ui-wizards/master/stable
+helm repo update
 ```
---
+++ hack/scripts/update-repo.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -eou pipefail
+
+SCRIPT_ROOT=$(realpath $(dirname "${BASH_SOURCE[0]}")/../..)
+SCRIPT_NAME=$(basename "${BASH_SOURCE[0]}")
+
+pushd $SCRIPT_ROOT
+
+# http://redsymbol.net/articles/bash-exit-traps/
+function cleanup() {
+    popd
+}
+trap cleanup EXIT
+
+CHARTS_DIR=charts
+TMP_DIR="${SCRIPT_ROOT}/tmp"
+
+REPO_DIR=${REPO_DIR:-stable}
+REPO_URL=https://raw.githubusercontent.com/bytebuilders/ui-wizards/master/${REPO_DIR}/
+
+# create temporary charts folder
+mkdir -p $TMP_DIR/$CHARTS_DIR
+# package charts
+cd $SCRIPT_ROOT
+
+find $CHARTS_DIR -maxdepth 1 -mindepth 1 -type d -exec helm package {} -d {} \;
+
+cd $CHARTS_DIR
+find . -maxdepth 1 -mindepth 1 -type d -exec mkdir -p $TMP_DIR/$CHARTS_DIR/{} \;
+find . -path ./$CHARTS_DIR -prune -o -name '*.tgz' -exec mv {} $TMP_DIR/$CHARTS_DIR/{} \;
+
+# update index
+cd $TMP_DIR
+mkdir -p $SCRIPT_ROOT/$REPO_DIR
+if [ -f $SCRIPT_ROOT/$REPO_DIR/index.yaml ]; then
+    helm repo index --merge $SCRIPT_ROOT/$REPO_DIR/index.yaml --url $REPO_URL $CHARTS_DIR
+else
+    helm repo index --url $REPO_URL $CHARTS_DIR
+fi
+mv $CHARTS_DIR/index.yaml $SCRIPT_ROOT/$REPO_DIR/index.yaml
+cd $CHARTS_DIR
+find . -maxdepth 1 -mindepth 1 -type d -exec mkdir -p $SCRIPT_ROOT/$REPO_DIR/{} \;
+find . -path ./$CHARTS_DIR -prune -o -name '*.tgz' -exec mv {} $SCRIPT_ROOT/$REPO_DIR/{} \;
+rm -rf $TMP_DIR
