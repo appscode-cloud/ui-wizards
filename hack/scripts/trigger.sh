bda56539e73b02dc577afc90ab2f3191ac9d34fd
Tamal Saha <tamal@appscode.com>
2021-05-07 11:59:49 -0700

    Fix linter warnings
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/scripts/trigger.sh
+++ hack/scripts/trigger.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Copyright AppsCode Inc. and Contributors
+#
+# Licensed under the AppsCode Free Trial License 1.0.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://github.com/appscode/licenses/raw/1.0.0/AppsCode-Free-Trial-1.0.0.md
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 set -eou pipefail
 
 SCRIPT_ROOT=$(realpath $(dirname "${BASH_SOURCE[0]}"))
@@ -33,6 +47,7 @@ repo_uptodate() {
 }
 
 echo "Update chart repo"
+make gen fmt
 ./hack/scripts/update-repo.sh
 
 if repo_uptodate; then
