6aed08d4a0c2c2cc75ad0957fac1f61146daffdf
Tamal Saha <tamal@appscode.com>
2021-06-30 19:15:12 -0700

    Fix options chart types
    Signed-off-by: Tamal Saha <tamal@appscode.com>

---
+++ apis/wizards/v1alpha1/types.go
@@ -0,0 +1,39 @@
+/*
+Copyright AppsCode Inc. and Contributors
+
+Licensed under the PolyForm Noncommercial License 1.0.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://github.com/appscode/licenses/raw/1.0.0/PolyForm-Noncommercial-1.0.0.md
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+// +kubebuilder:validation:Enum=db.t.micro;db.t.small;db.t.medium;db.t.large;db.t.xlarge;db.t.2xlarge;db.m.small;db.m.large;db.m.xlarge;db.m.2xlarge;db.m.4xlarge;db.m.8xlarge;db.m.12xlarge;db.m.16xlarge;db.m.24xlarge;db.r.large;db.r.xlarge;db.r.2xlarge;db.r.4xlarge;db.r.8xlarge;db.r.12xlarge;db.r.16xlarge;db.r.24xlarge
+type MachineType string
+
+// +kubebuilder:validation:Enum=Halt;Delete;WipeOut;DoNotTerminate
+type TerminationPolicy string
+
+type Persistence struct {
+	Size string `json:"size"`
+}
+
+type StorageClass struct {
+	Name string `json:"name"`
+}
+
+type AuthSecret struct {
+	// +optional
+	Name string `json:"name"`
+	// +optional
+	// +kubebuilder:validation:Format:=password
+	Password string `json:"password"`
+}
