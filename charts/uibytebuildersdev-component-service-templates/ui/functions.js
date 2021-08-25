7c65b21fb373ad000ac869e1a1557d1a04fc3e28
shakib779 <junayed@appscode.com>
2021-08-25 11:41:11 +0600

    Added mysql editor wizards yaml (option, create, edit, opsrequest) (#94)
    Signed-off-by: shakib779 <junayed@appscode.com>

---
+++ charts/uibytebuildersdev-component-service-templates/ui/functions.js
@@ -0,0 +1,11 @@
+function isEqualToServiceMonitorType(
+    { rootModel, watchDependency },
+    value
+  ) {
+    watchDependency("rootModel#/spec/type");
+    return rootModel && rootModel.spec && rootModel.spec.type === value;
+  }
+
+return {
+    isEqualToServiceMonitorType
+}
