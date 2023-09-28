a09651080ec73155be705be0f662b85147a5ef84
Nihal-Azmain <nihal@appscode.com>
2023-09-28 15:33:45 +0600

    Resolved review changes
    Signed-off-by: Nihal-Azmain <nihal@appscode.com>

--- charts/kubedbcom-redis-editor-options/ui/functions.js
+++ charts/kubedbcom-redis-editor-options/ui/functions.js
@@ -613,7 +613,6 @@ const ifZones = ({ model, getValue, watchDependency }) => {
 
 const zonesOnChange = ({ model, getValue, commit }) => {
   const zones = getValue(model, "form/capi/zones") || [];
-  const isDedicated = getValue(model, "form/capi/dedicated");
   if (!zones.length) commit("wizard/model$delete", "form/capi/sku");
 };
 
@@ -651,8 +650,7 @@ async function getSKU({storeGet,axios,model,getValue,watchDependency}) {
       url = url.slice(0,-1)
       const resp = await axios.get(url);
       const val = resp.data.map((item)=>{
-        console.log(item)
-        return {"value":item.name,"text":item.name}
+        return {"value":item.name,"text":`${item.name} [CPU: ${item.cpu}] [Memory: ${item.memory}mb] `}
       })
       return val
     } catch (e) {
