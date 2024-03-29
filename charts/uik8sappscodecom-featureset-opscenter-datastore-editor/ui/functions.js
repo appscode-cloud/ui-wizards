dc35e2752c7d6d6c513532458ebf866bb7b8afb4
SamiulSourav <samiul@appscode.com>
2024-03-29 12:03:22 +0600

    database type selection in cluster-ui featureset
    Signed-off-by: SamiulSourav <samiul@appscode.com>

--- charts/uik8sappscodecom-featureset-opscenter-datastore-editor/ui/functions.js
+++ charts/uik8sappscodecom-featureset-opscenter-datastore-editor/ui/functions.js
@@ -380,6 +380,62 @@ function checkIsResourceLoaded ({commit, storeGet,watchDependency,getValue,discr
   }
 }
 
+function isKubedbSelected({ getValue, discriminator, watchDependency, commit }){
+  watchDependency("discriminator#/enabledFeatures");
+  const enabledFeatures = getValue(discriminator, "/enabledFeatures");
+  const target = "kubedb";
+  const isSelected = enabledFeatures?.includes(target);
+  if(!isSelected) commit("wizard/model$delete", "resources/helmToolkitFluxcdIoHelmRelease_kubedb")
+  return isSelected;
+}
+
+const allAvailableTypes = [
+  "Druid",
+  "Elasticsearch",
+  "FerretDB",
+  "Kafka",
+  "MariaDB",
+  "Memcached",
+  "MicrosoftSQLServer",
+  "MongoDB",
+  "MySQL",
+  "PerconaXtraDB",
+  "PgBouncer",
+  "Pgpool",
+  "Postgres",
+  "ProxySQL",
+  "RabbitMQ",
+  "Redis",
+  "Singlestore",
+  "Solr",
+  "ZooKeeper",
+];
+function getDatabaseTypes({ setDiscriminatorValue, commit }) {
+  const preEnableTypes = ["Elasticsearch", "Kafka", "MariaDB", "MongoDB", "MySQL", "Postgres", "Redis"];
+  setDiscriminatorValue("/enabledTypes", preEnableTypes);
+  typeConvert(commit, preEnableTypes);
+  return allAvailableTypes;
+}
+
+function onTypeUpdate({ discriminator, commit, getValue }) {
+  const enabledTypes = getValue(discriminator, "/enabledTypes");
+  typeConvert(commit, enabledTypes);
+}
+
+function typeConvert(commit, enabledTypes) {
+  let convertFromArray = {};
+  allAvailableTypes?.forEach(item => {
+    convertFromArray[item] = enabledTypes.includes(item);
+  });
+  commit("wizard/model$update", {
+    path: "resources/helmToolkitFluxcdIoHelmRelease_kubedb/spec/values/global/featureGates",
+    value: convertFromArray,
+    force: true,
+  });
+  console.log(convertFromArray);
+  return convertFromArray;
+}
+
 return {
   hideThisElement,
   checkIsResourceLoaded,
@@ -393,4 +449,8 @@ return {
   returnFalse,
   setReleaseNameAndNamespaceAndInitializeValues,
   fetchFeatureSetOptions,
+  isKubedbSelected,
+  getDatabaseTypes,
+  onTypeUpdate,
+  typeConvert,
 };
