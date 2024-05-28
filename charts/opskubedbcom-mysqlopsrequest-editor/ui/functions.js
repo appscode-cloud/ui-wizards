26e342ffe28cf36c2a900708ad78c0b6b31c1543
SamiulSourav <samiul@appscode.com>
2024-05-28 18:14:57 +0600

    Add applyConfig in mysql, postgress, redis
    Signed-off-by: SamiulSourav <samiul@appscode.com>

--- charts/opskubedbcom-mysqlopsrequest-editor/ui/functions.js
+++ charts/opskubedbcom-mysqlopsrequest-editor/ui/functions.js
@@ -531,10 +531,31 @@ function ifReconfigurationTypeEqualsTo(
 
   return reconfigurationType === value;
 }
+
+function onApplyconfigChange({ discriminator, getValue, commit }) {
+  const applyconfig = getValue(discriminator, "/applyconfig");
+
+  const configObj = {};
+
+  if (applyconfig) {
+    applyconfig.forEach((item) => {
+      const { key, value } = item;
+      configObj[key] = value;
+    });
+  }
+
+  commit("wizard/model$update", {
+    path: "/spec/configuration/applyConfig",
+    value: configObj,
+    force: true,
+  });
+}
+
 function onReconfigurationTypeChange(
-  { commit, discriminator, getValue }
+  { commit, discriminator, getValue, setDiscriminatorValue }
 ) {
   const reconfigurationType = getValue(discriminator, "/reconfigurationType");
+  setDiscriminatorValue("/applyconfig", {});
   if (reconfigurationType === "remove") {
     commit("wizard/model$delete", `/spec/configuration`);
 
@@ -550,25 +571,13 @@ function onReconfigurationTypeChange(
     );
     commit(
       "wizard/model$delete",
-      `/spec/configuration/inlineConfig`
+      `/spec/configuration/applyConfig`
     );
     commit(
       "wizard/model$delete",
       `/spec/configuration/removeCustomConfig`
     );
   }
-}
-function disableReconfigurationType(
-  { getValue, watchDependency, discriminator, itemCtx },
-) {
-  watchDependency("discriminator#/dbDetails");
-  const dbDetails = getValue(discriminator, "/dbDetails");
-
-  const { spec } = dbDetails || {};
-    if (itemCtx.value === "inlineConfig" || itemCtx.value === "remove") {
-      if (spec.configSecret) return false;
-      else return true;
-    } else return false;
 }
 
 // for tls
@@ -835,7 +844,7 @@ return {
   unNamespacedResourceNames,
   ifReconfigurationTypeEqualsTo,
   onReconfigurationTypeChange,
-  disableReconfigurationType,
+  onApplyconfigChange,
   hasTlsField,
   initIssuerRefApiGroup,
   getIssuerRefsName,
--- charts/opskubedbcom-redisopsrequest-editor/ui/functions.js
+++ charts/opskubedbcom-redisopsrequest-editor/ui/functions.js
@@ -524,10 +524,31 @@ function ifReconfigurationTypeEqualsTo(
 
   return reconfigurationType === value;
 }
+
+function onApplyconfigChange({ discriminator, getValue, commit }) {
+  const applyconfig = getValue(discriminator, "/applyconfig");
+
+  const configObj = {};
+  if (applyconfig) {
+    applyconfig.forEach((item) => {
+      const { key, value } = item;
+      configObj[key] = value;
+    });
+  }
+  
+  console.log(configObj);
+  commit("wizard/model$update", {
+    path: "/spec/configuration/applyConfig",
+    value: configObj,
+    force: true,
+  });
+}
+
 function onReconfigurationTypeChange(
-  { commit, discriminator, getValue }
+  { commit, discriminator, getValue, setDiscriminatorValue }
 ) {
   const reconfigurationType = getValue(discriminator, "/reconfigurationType");
+  setDiscriminatorValue("/applyconfig", {});
   if (reconfigurationType === "remove") {
     commit("wizard/model$delete", `/spec/configuration`);
 
@@ -543,25 +564,13 @@ function onReconfigurationTypeChange(
     );
     commit(
       "wizard/model$delete",
-      `/spec/configuration/inlineConfig`
+      `/spec/configuration/applyConfig`
     );
     commit(
       "wizard/model$delete",
       `/spec/configuration/removeCustomConfig`
     );
   }
-}
-function disableReconfigurationType(
-  { getValue, watchDependency, discriminator, itemCtx },
-) {
-  watchDependency("discriminator#/dbDetails");
-  const dbDetails = getValue(discriminator, "/dbDetails");
-
-  const { spec } = dbDetails || {};
-    if (itemCtx.value === "inlineConfig" || itemCtx.value === "remove") {
-      if (spec.configSecret) return false;
-      else return true;
-    } else return false;
 }
 
 // for tls
@@ -812,7 +821,7 @@ return {
   unNamespacedResourceNames,
   ifReconfigurationTypeEqualsTo,
   onReconfigurationTypeChange,
-  disableReconfigurationType,
+  onApplyconfigChange,
   hasTlsField,
   initIssuerRefApiGroup,
   getIssuerRefsName,
