749dcc1153feaf02b5ad67301237ff69b735d98c
SayedTahsin <mail.tahsin99@gmail.com>
2024-07-30 12:00:57 +0600

    create UI completed function.js updated
    Signed-off-by: SayedTahsin <mail.tahsin99@gmail.com>

--- charts/kubedbcom-perconaxtradb-editor-options/ui/functions.js
+++ charts/kubedbcom-perconaxtradb-editor-options/ui/functions.js
@@ -787,65 +787,18 @@ function clearConfiguration({ discriminator, getValue, commit }) {
     commit("wizard/model$delete", "/spec/configuration");
   }
 }
-
-function setStorageClass({ model, getValue, commit }) {
-  const deletionPolicy = getValue(model, "/spec/deletionPolicy") || "";
-  let storageClass =
-    getValue(model, "/spec/admin/storageClasses/default") || "";
-  const storageClassList =
-    getValue(model, "/spec/admin/storageClasses/available") || [];
-  const suffix = "-retain";
-
-  const simpleClassList = storageClassList.filter((item) => {
-    return !item.endsWith(suffix);
-  });
-  const retainClassList = storageClassList.filter((item) => {
-    return item.endsWith(suffix);
-  });
-  if (deletionPolicy === "WipeOut" || deletionPolicy === "Delete") {
-    storageClass = simpleClassList.length
-      ? simpleClassList[0]
-      : retainClassList[0];
-  } else {
-    storageClass = retainClassList.length
-      ? retainClassList[0]
-      : simpleClassList[0];
-  }
-
-  const isChangeable = isToggleOn({ getValue, model }, "storageClasses");
-  if (isChangeable && storageClass) {
+function setResourceLimit({ commit, model, getValue, watchDependency }) {
+  const modelPathValue = getValue(model, "/spec/podResources/machine");
+  watchDependency("model#/spec/podResources/machine");
+  if (modelPathValue && modelPathValue !== "custom") {
+    // to avoiding set value by reference, cpu and memory set separately
     commit("wizard/model$update", {
-      path: "/spec/admin/storageClasses/default",
-      value: storageClass,
+      path: "/spec/podResources/resources",
+      value: machines[modelPathValue]?.resources,
       force: true,
     });
   }
 }
-
-function getAdminOptions({ getValue, model }, type) {
-  const options = getValue(model, `/spec/admin/${type}/available`) || [];
-  return options;
-}
-
-function isToggleOn({ getValue, model }, type) {
-  return getValue(model, `/spec/admin/${type}/toggle`);
-}
-
-function showAlerts({ watchDependency, model, getValue, discriminator }) {
-  watchDependency("discriminator#/monitoring");
-  const isMonitorEnabled = getValue(discriminator, "/monitoring");
-  return isMonitorEnabled && isToggleOn({ getValue, model }, "alerts");
-}
-
-function onBackupSwitch({ discriminator, getValue, commit }) {
-  const isBackupOn = getValue(discriminator, "/backup");
-  commit("wizard/model$update", {
-    path: "/spec/admin/backup/tool",
-    value: isBackupOn ? "KubeStash" : "",
-    force: true,
-  });
-}
-
 function clearArbiterHidden({ commit }) {
   commit("wizard/model$update", {
     path: `/spec/arbiter/enabled`,
@@ -859,76 +812,73 @@ function clearArbiterHidden({ commit }) {
     force: true,
   });
 }
-
-async function getNodeTopology({
-  model,
-  getValue,
-  axios,
-  storeGet,
-  watchDependency,
-}) {
-  watchDependency("model#/spec/admin/deployment/default");
-  watchDependency("model#/spec/admin/clusterTier/default");
-  const owner = storeGet("/route/params/user");
-  const cluster = storeGet("/route/params/cluster");
-  const deploymentType =
-    getValue(model, "/spec/admin/deployment/default") || "";
-  const clusterTier = getValue(model, "/spec/admin/clusterTier/default") || "";
-  const nodeTopologyList =
-    getValue(model, `/spec/admin/clusterTier/nodeTopology/available`) || [];
-  let mappedResp = [];
-  if (nodeTopologyListFromApi.length === 0) {
-    try {
-      const url = `/clusters/${owner}/${cluster}/proxy/node.k8s.appscode.com/v1alpha1/nodetopologies`;
-      const resp = await axios.get(url);
-      nodeTopologyListFromApi = resp.data?.items;
-      const filteredResp = resp.data?.items.filter(
-        (item) =>
-          item.metadata.labels?.["node.k8s.appscode.com/tenancy"] ===
-          deploymentType.toLowerCase()
-      );
-      mappedResp = filteredResp?.map((item) => {
-        const name = (item.metadata && item.metadata.name) || "";
-        return name;
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  } else {
-    const filteredResp = nodeTopologyListFromApi.filter(
-      (item) =>
-        item.metadata.labels?.["node.k8s.appscode.com/tenancy"] ===
-        deploymentType.toLowerCase()
-    );
-    mappedResp = filteredResp?.map((item) => {
-      const name = (item.metadata && item.metadata.name) || "";
-      return name;
-    });
-  }
-
-  const statusUrl = `/clustersv2/${owner}/${cluster}/status`;
-  if (provider.length === 0) {
-    try {
-      const resp = await axios.get(statusUrl);
-      provider = resp.data?.provider;
-    } catch (e) {
-      console.log(e);
-    }
-  }
-  const filteredList = filterNodeTopology(
-    nodeTopologyList,
-    clusterTier,
-    provider,
-    mappedResp
-  );
-  return filteredList;
-}
 function returnFalse() {
   return false;
 }
-function isConfigDatabaseOn({ watchDependency, discriminator, getValue }) {
-  watchDependency("discriminator#/configDatabase");
-  return getValue(discriminator, "/configDatabase");
+function showHidden({ watchDependency, model, getValue }) {
+  watchDependency("model#/spec/hidden/enabled");
+  const isHiddenOn = getValue(model, "/spec/hidden/enabled") || "";
+  const notStandalone = notEqualToDatabaseMode(
+    { model, getValue, watchDependency },
+    "Standalone"
+  );
+  return isHiddenOn && notStandalone;
+}
+function showArbiter({ watchDependency, model, getValue }) {
+  watchDependency("model#/spec/arbiter/enabled");
+  const isArbiterOn = getValue(model, "/spec/arbiter/enabled") || "";
+  const notStandalone = notEqualToDatabaseMode(
+    { model, getValue, watchDependency },
+    "Standalone"
+  );
+  return isArbiterOn && notStandalone;
+}
+async function fetchJsons({ axios, itemCtx }) {
+  let ui = {};
+  let language = {};
+  let functions = {};
+  const { name, sourceRef, version, packageviewUrlPrefix } = itemCtx.chart;
+
+  try {
+    ui = await axios.get(
+      `${packageviewUrlPrefix}/create-ui.yaml?name=${name}&sourceApiGroup=${sourceRef.apiGroup}&sourceKind=${sourceRef.kind}&sourceNamespace=${sourceRef.namespace}&sourceName=${sourceRef.name}&version=${version}&format=json`
+    );
+    language = await axios.get(
+      `${packageviewUrlPrefix}/language.yaml?name=${name}&sourceApiGroup=${sourceRef.apiGroup}&sourceKind=${sourceRef.kind}&sourceNamespace=${sourceRef.namespace}&sourceName=${sourceRef.name}&version=${version}&format=json`
+    );
+    const functionString = await axios.get(
+      `${packageviewUrlPrefix}/functions.js?name=${name}&sourceApiGroup=${sourceRef.apiGroup}&sourceKind=${sourceRef.kind}&sourceNamespace=${sourceRef.namespace}&sourceName=${sourceRef.name}&version=${version}`
+    );
+    // declare evaluate the functionString to get the functions Object
+    const evalFunc = new Function(functionString.data || "");
+    functions = evalFunc();
+  } catch (e) {
+    console.log(e);
+  }
+
+  return {
+    ui: ui.data || {},
+    language: language.data || {},
+    functions,
+  };
+}
+
+function isEqualToModelPathValue(
+  { model, getValue, watchDependency },
+  value,
+  modelPath
+) {
+  const modelPathValue = getValue(model, modelPath);
+  watchDependency("model#" + modelPath);
+  return modelPathValue === value;
+}
+
+function showAuthSecretField({ discriminator, getValue, watchDependency }) {
+  return !showAuthPasswordField({
+    discriminator,
+    getValue,
+    watchDependency,
+  });
 }
 function notEqualToDatabaseMode({ model, getValue, watchDependency }, mode) {
   const modelPathValue = getValue(model, "/spec/mode");
@@ -941,226 +891,195 @@ function showStorageSizeField({ model, getValue, watchDependency }) {
   const validType = [];
   return !validType.includes(modelPathValue);
 }
-function showHidden({ watchDependency, model, getValue }) {
-  watchDependency("model#/spec/hidden/enabled");
-  const isHiddenOn = getValue(model, "/spec/hidden/enabled") || "";
-  const notStandalone = notEqualToDatabaseMode(
-    { model, getValue, watchDependency },
-    "Standalone"
-  );
-  return isHiddenOn && notStandalone;
-}
-function notEqualToDatabaseMode({ model, getValue, watchDependency }, mode) {
-  const modelPathValue = getValue(model, "/spec/mode");
-  watchDependency("model#/spec/mode");
-  return modelPathValue && modelPathValue !== mode;
-}
-function showArbiter({ watchDependency, model, getValue }) {
-  watchDependency("model#/spec/arbiter/enabled");
-  const isArbiterOn = getValue(model, "/spec/arbiter/enabled") || "";
-  const notStandalone = notEqualToDatabaseMode(
-    { model, getValue, watchDependency },
-    "Standalone"
-  );
-  return isArbiterOn && notStandalone;
-}
-function clearConfiguration({ discriminator, getValue, commit }) {
-  const configOn = getValue(discriminator, "/configDatabase");
 
-  if (!configOn) {
-    commit("wizard/model$delete", "/spec/configuration");
-  }
-}
-function filterNodeTopology(list, tier, provider, map) {
-  // first filter the list from value that exists from the filtered list got from API
-  const filteredlist = list.filter((item) => {
-    return map.includes(item);
-  });
-
-  // filter the list based on clusterTier
-  if (provider === "EKS") {
-    return filteredlist.filter((item) => {
-      if (tier === "CPUOptimized") return item.startsWith("c");
-      else if (tier === "MemoryOptimized") return item.startsWith("r");
-      else return !item.startsWith("c") && !item.startsWith("r");
-    });
-  } else if (provider === "AKS") {
-    return filteredlist.filter((item) => {
-      if (tier === "CPUOptimized")
-        return item.startsWith("f") || item.startsWith("fx");
-      else if (tier === "MemoryOptimized")
-        return (
-          item.startsWith("e") ||
-          item.startsWith("eb") ||
-          item.startsWith("ec") ||
-          item.startsWith("m") ||
-          item.startsWith("d")
-        );
-      else
-        return (
-          !(item.startsWith("f") || item.startsWith("fx")) &&
-          !(
-            item.startsWith("e") ||
-            item.startsWith("eb") ||
-            item.startsWith("ec") ||
-            item.startsWith("m") ||
-            item.startsWith("d")
-          )
-        );
-    });
-  } else if (provider === "GKE") {
-    return filteredlist.filter((item) => {
-      if (tier === "CPUOptimized")
-        return (
-          item.startsWith("h3") ||
-          item.startsWith("c2") ||
-          item.startsWith("c2d")
-        );
-      else if (tier === "MemoryOptimized")
-        return (
-          item.startsWith("x4") ||
-          item.startsWith("m1") ||
-          item.startsWith("m2") ||
-          item.startsWith("m3")
-        );
-      else
-        return (
-          !(
-            item.startsWith("h3") ||
-            item.startsWith("c2") ||
-            item.startsWith("c2d")
-          ) &&
-          !(
-            item.startsWith("x4") ||
-            item.startsWith("m1") ||
-            item.startsWith("m2") ||
-            item.startsWith("m3")
-          )
-        );
-    });
-  }
-}
-function showIssuer({ model, getValue, watchDependency }) {
-  watchDependency("model#/spec/admin/tls/default");
-  const isTlsEnabled = getValue(model, "/spec/admin/tls/default");
-  const isIssuerToggleEnabled = isToggleOn(
-    { getValue, model },
-    "clusterIssuers"
-  );
-  return isTlsEnabled && isIssuerToggleEnabled;
-}
-function onAuthChange({ getValue, discriminator, commit }) {
-  const isAuthOn = getValue(discriminator, "/createAuthSecret");
-  if (!isAuthOn) {
-    commit("wizard/model$update", {
-      path: "/spec/authSecret/name",
-      value: "",
-      force: true,
-    });
-    commit("wizard/model$update", {
-      path: "/spec/authSecret/password",
-      value: "",
-      force: true,
-    });
-  }
-}
-function setMonitoring({ getValue, model }) {
-  const agent = getValue(model, "/spec/admin/monitoring/agent") || "";
-  return !!agent;
-}
-async function isBackupCluster({ axios, storeGet, commit }) {
+async function getResources({ axios, storeGet }, group, version, resource) {
   const owner = storeGet("/route/params/user");
   const cluster = storeGet("/route/params/cluster");
-  const url = `/clusters/${owner}/${cluster}/proxy/ui.k8s.appscode.com/v1alpha1/features`;
-  let isStashEnabled = false;
 
-  try {
-    const resp = await axios.get(url);
-    const stashPreset = resp.data?.items?.find(
-      (item) => item.metadata?.name === "stash-presets"
-    );
-    isStashEnabled = !!(
-      stashPreset?.status?.enabled && stashPreset?.status?.ready
-    );
-  } catch (e) {
-    console.log(e);
-  }
-  commit("wizard/model$update", {
-    path: "/spec/admin/backup/tool",
-    value: isStashEnabled ? "KubeStash" : "",
-    force: true,
+  const resp = await axios.get(
+    `/clusters/${owner}/${cluster}/proxy/${group}/${version}/${resource}`,
+    {
+      params: { filter: { items: { metadata: { name: null } } } },
+    }
+  );
+
+  const resources = (resp && resp.data && resp.data.items) || [];
+
+  resources.map((item) => {
+    const name = (item.metadata && item.metadata.name) || "";
+    item.text = name;
+    item.value = name;
+    return true;
   });
-  return isStashEnabled;
+  return resources;
 }
-function isMachineNotCustom({ model, getValue, watchDependency }, path) {
-  const fullpath = path
-    ? `/spec/${path}/podResources/machine`
-    : "/spec/podResources/machine";
-  const modelPathValue = getValue(model, fullpath);
-  watchDependency(`model#${fullpath}`);
+
+async function getStorageClassNames({
+  axios,
+  storeGet,
+  commit,
+  model,
+  getValue,
+}) {
+  const owner = storeGet("/route/params/user");
+  const cluster = storeGet("/route/params/cluster");
+
+  const resp = await axios.get(
+    `/clusters/${owner}/${cluster}/proxy/storage.k8s.io/v1/storageclasses`,
+    {
+      params: {
+        filter: { items: { metadata: { name: null, annotations: null } } },
+      },
+    }
+  );
+
+  const resources = (resp && resp.data && resp.data.items) || [];
+
+  resources.map((item) => {
+    const name = (item.metadata && item.metadata.name) || "";
+    item.text = name;
+    item.value = name;
+    return true;
+  });
+  storageClassList = resources;
+  setStorageClass({ model, getValue, commit });
+  return resources;
+}
+async function getMongoDbVersions(
+  { axios, storeGet },
+  group,
+  version,
+  resource
+) {
+  const owner = storeGet("/route/params/user");
+  const cluster = storeGet("/route/params/cluster");
+
+  const queryParams = {
+    filter: {
+      items: {
+        metadata: { name: null },
+        spec: { version: null, deprecated: null },
+      },
+    },
+  };
+
+  const resp = await axios.get(
+    `/clusters/${owner}/${cluster}/proxy/${group}/${version}/${resource}`,
+    {
+      params: queryParams,
+    }
+  );
+
+  const resources = (resp && resp.data && resp.data.items) || [];
+
+  // keep only non deprecated versions
+  const filteredMongoDbVersions = resources.filter(
+    (item) => item.spec && !item.spec.deprecated
+  );
+
+  filteredMongoDbVersions.map((item) => {
+    const name = (item.metadata && item.metadata.name) || "";
+    const specVersion = (item.spec && item.spec.version) || "";
+    item.text = `${name} (${specVersion})`;
+    item.value = name;
+    return true;
+  });
+  return filteredMongoDbVersions;
+}
+function onCreateAuthSecretChange({ discriminator, getValue, commit }) {
+  const createAuthSecret = getValue(discriminator, "/createAuthSecret");
+  if (createAuthSecret) {
+    commit("wizard/model$delete", "/spec/authSecret/name");
+  } else if (createAuthSecret === false) {
+    commit("wizard/model$delete", "/spec/authSecret/password");
+  }
+}
+
+async function getSecrets({
+  storeGet,
+  axios,
+  model,
+  getValue,
+  watchDependency,
+}) {
+  const owner = storeGet("/route/params/user");
+  const cluster = storeGet("/route/params/cluster");
+  const namespace = getValue(model, "/metadata/release/namespace");
+  watchDependency("model#/metadata/release/namespace");
+
+  const resp = await axios.get(
+    `/clusters/${owner}/${cluster}/proxy/core/v1/namespaces/${namespace}/secrets`,
+    {
+      params: {
+        filter: { items: { metadata: { name: null }, type: null } },
+      },
+    }
+  );
+
+  const secrets = (resp && resp.data && resp.data.items) || [];
+
+  const filteredSecrets = secrets.filter((item) => {
+    const validType = ["kubernetes.io/service-account-token", "Opaque"];
+    return validType.includes(item.type);
+  });
+
+  filteredSecrets.map((item) => {
+    const name = (item.metadata && item.metadata.name) || "";
+    item.text = name;
+    item.value = name;
+    return true;
+  });
+  return filteredSecrets;
+}
+
+function disableLimit({ model, getValue, watchDependency }) {
+  const modelPathValue = getValue(model, "/spec/podResources/machine");
+  watchDependency("model#/spec/podResources/machine");
   return modelPathValue !== "custom" && !!modelPathValue;
 }
-async function getNamespaces({ axios, storeGet }) {
-  const params = storeGet("/route/params");
-  const { user, cluster, group, version, resource } = params;
-  try {
-    const resp = await axios.post(
-      `/clusters/${user}/${cluster}/proxy/identity.k8s.appscode.com/v1alpha1/selfsubjectnamespaceaccessreviews`,
-      {
-        apiVersion: "identity.k8s.appscode.com/v1alpha1",
-        kind: "SelfSubjectNamespaceAccessReview",
-        spec: {
-          resourceAttributes: [
-            {
-              verb: "create",
-              group: group,
-              version: version,
-              resource: resource,
-            },
-          ],
-        },
-      }
-    );
-    const namespaces = resp?.data?.status?.namespaces || [];
-    return namespaces;
-  } catch (e) {
-    console.log(e);
-    return [];
-  }
-}
-function updateAlertValue({ commit, model, discriminator, getValue }) {
-  const isMonitorEnabled = getValue(discriminator, "/monitoring");
-  const alert = isMonitorEnabled ? "warning" : "none";
+
+function updateAgentValue({ commit }, val) {
+  commit("wizard/model$update", {
+    path: "/spec/monitoring/agent",
+    value: val ? "prometheus.io/operator" : "",
+    force: true,
+  });
+
   // update alert value depend on monitoring profile
   commit("wizard/model$update", {
     path: "/form/alert/enabled",
-    value: alert,
-    force: true,
-  });
-  const agent = isMonitorEnabled ? "prometheus.io/operator" : "";
-  commit("wizard/model$update", {
-    path: "/spec/admin/monitoring/agent",
-    value: agent,
+    value: val ? "warning" : "none",
     force: true,
   });
 }
-function setResourceLimit({ commit, model, getValue, watchDependency }) {
-  const modelPathValue = getValue(model, "/spec/podResources/machine");
-  watchDependency("model#/spec/podResources/machine");
-  if (modelPathValue && modelPathValue !== "custom") {
-    // to avoiding set value by reference, cpu and memory set separately
+
+function showReplicaField({ model, getValue, watchDependency }) {
+  const modelPathValue = getValue(model, "/spec/mode");
+  watchDependency("model#/spec/mode");
+  const validType = ["Replicaset"];
+  return validType.includes(modelPathValue);
+}
+
+function onModeChange({ model, getValue, watchDependency, commit }) {
+  const modelPathValue = getValue(model, "/spec/mode");
+  watchDependency("model#/spec/mode");
+  if (modelPathValue === "Replicaset") {
     commit("wizard/model$update", {
-      path: "/spec/podResources/resources",
-      value: machines[modelPathValue]?.resources,
+      path: "/spec/replicas",
+      value: 3,
+      force: true,
+    });
+  } else {
+    commit("wizard/model$update", {
+      path: "/spec/replicas",
+      value: 1,
       force: true,
     });
   }
 }
-
 return {
   getNamespaces,
   updateAlertValue,
-  setStorageClass,
   getAdminOptions,
   isToggleOn,
   showAlerts,
@@ -1178,7 +1097,6 @@ return {
   showIssuer,
   showArbiter,
   clearConfiguration,
-  showStorageSizeField,
   onBackupSwitch,
   isVariantAvailable,
   fetchJsons,
