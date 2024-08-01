3d0065a6d113851d1526e8b7629bd3533ecdf83c
SayedTahsin <mail.tahsin99@gmail.com>
2024-08-01 16:55:47 +0600

    proxySql fix
    Signed-off-by: SayedTahsin <mail.tahsin99@gmail.com>

--- charts/kubedbcom-proxysql-editor-options/ui/functions.js
+++ charts/kubedbcom-proxysql-editor-options/ui/functions.js
@@ -1084,7 +1084,14 @@ function isMachineNotCustom({ model, getValue, watchDependency }, path) {
   return modelPathValue !== "custom" && !!modelPathValue;
 }
 
+function EqualToDatabaseMode({ model, getValue, watchDependency }, mode) {
+  const modelPathValue = getValue(model, "/spec/mode");
+  watchDependency("model#/spec/mode");
+  return modelPathValue && modelPathValue === mode;
+}
+
 return {
+  EqualToDatabaseMode,
   getNamespaces,
   updateAlertValue,
   setStorageClass,
