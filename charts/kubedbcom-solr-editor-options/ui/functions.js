ca346d23e08a945f50cf259cf8e09df0967a7480
Sayed Tashinoor Rahman <75539185+SayedTahsin@users.noreply.github.com>
2024-08-07 18:36:19 +0600

    Machine resources (#537)
    * Support for topology cluster
    
    Signed-off-by: Arnob kumar saha <arnob@appscode.com>
    
    * machine setResources on topology
    
    Signed-off-by: SayedTahsin <mail.tahsin99@gmail.com>
    
    * sharded cluser machine resource
    
    Signed-off-by: SayedTahsin <mail.tahsin99@gmail.com>
    
    * Singlestore machine
    
    Signed-off-by: SayedTahsin <mail.tahsin99@gmail.com>
    
    * Solr zookeeperRef
    
    Signed-off-by: SayedTahsin <mail.tahsin99@gmail.com>
    
    * Fix solr autoscaler
    
    Signed-off-by: Arnob kumar saha <arnob@appscode.com>
    
    ---------
    
    Signed-off-by: Arnob kumar saha <arnob@appscode.com>
    Signed-off-by: SayedTahsin <mail.tahsin99@gmail.com>
    Co-authored-by: Arnob kumar saha <arnob@appscode.com>
    Co-authored-by: Al Asad Nur Riyad <asadnurriyad@appscode.com>

--- charts/kubedbcom-solr-editor-options/ui/functions.js
+++ charts/kubedbcom-solr-editor-options/ui/functions.js
@@ -449,6 +449,43 @@ function getMachineListForOptions() {
   return array
 }
 
+function setResourceLimitOnModeChange({ commit, model, getValue }) {
+  let modelPathValue = getValue(model, `/spec/mode`)
+  if (modelPathValue === 'Topology') {
+    setResourceLimitTopology({ commit, model, getValue }, 'coordinator')
+    setResourceLimitTopology({ commit, model, getValue }, 'data')
+    setResourceLimitTopology({ commit, model, getValue }, 'overseer')
+  }
+}
+
+function setResourceLimitTopology({ commit, model, getValue, watchDependency }, topology) {
+  let modelPathValue = getValue(model, `/spec/topology/${topology}/podResources/machine`)
+  const deploymentType = getValue(model, '/spec/admin/deployment/default')
+
+  if (modelPathValue) {
+    if (modelPathValue === 'custom') modelPathValue = 'db.t.micro'
+    // to avoiding set value by reference, cpu and memory set separately
+    if (deploymentType === 'Dedicated') {
+      commit('wizard/model$update', {
+        path: `/spec/topology/${topology}/podResources/resources/requests`,
+        value: machines[modelPathValue]?.resources.limits,
+        force: true,
+      })
+      commit('wizard/model$update', {
+        path: `/spec/topology/${topology}/podResources/resources/limits`,
+        value: machines[modelPathValue]?.resources.limits,
+        force: true,
+      })
+    } else {
+      commit('wizard/model$update', {
+        path: `/spec/topology/${topology}/podResources/resources`,
+        value: machines[modelPathValue]?.resources,
+        force: true,
+      })
+    }
+  }
+}
+
 function setResourceLimit({ commit, model, getValue, watchDependency }) {
   let modelPathValue = getValue(model, '/spec/podResources/machine')
   const deploymentType = getValue(model, '/spec/admin/deployment/default')
@@ -957,7 +994,8 @@ async function isBackupCluster({ axios, storeGet, commit }) {
   return isStashEnabled
 }
 
-async function getAppBindings({ axios, storeGet }) {
+async function getAppBindings({ commit, axios, storeGet, model, getValue }) {
+  const namespace = getValue(model, '/metadata/release/namespace')
   const owner = storeGet('/route/params/user')
   const cluster = storeGet('/route/params/cluster')
 
@@ -969,7 +1007,7 @@ async function getAppBindings({ axios, storeGet }) {
       },
     },
   }
-
+  setNamespace({ commit, model, getValue })
   try {
     const resp = await axios.get(
       `/clusters/${owner}/${cluster}/proxy/appcatalog.appscode.com/v1alpha1/appbindings`,
@@ -985,7 +1023,7 @@ async function getAppBindings({ axios, storeGet }) {
       .map((item) => {
         const name = (item.metadata && item.metadata.name) || ''
         return {
-          text: name,
+          text: `${namespace}/${name}`,
           value: name,
         }
       })
@@ -1008,6 +1046,8 @@ function setNamespace({ commit, model, getValue }) {
 }
 
 return {
+  setResourceLimitOnModeChange,
+  setResourceLimitTopology,
   setNamespace,
   getAppBindings,
   isVariantAvailable,
