01f450b5b668cfc2443d79f4904963abeddb7114
Nihal Azmain <nihal@appscode.com>
2024-08-09 18:36:29 +0600

    Add kubedb part in create-ui
    Signed-off-by: Nihal Azmain <nihal@appscode.com>

--- charts/chartsxhelmdev-clusterchartpreset-editor-options/ui/functions.js
+++ charts/chartsxhelmdev-clusterchartpreset-editor-options/ui/functions.js
@@ -1,199 +1,66 @@
-function getOptions({ getValue, model }, type) {
-  const options = getValue(model, `/spec/kubeDB/${type}/available`) || []
+function getOptions({ getValue, model, watchDependency }, type) {
+  watchDependency(`model#/spec/kubeDB/${type}/available`)
+  const options = getValue(model, `/spec/kubeDB/${type}/available`)
   return options
 }
 
-let nodeTopologyListFromApi = []
-let nodeTopologyApiCalled = false
-let provider = ''
-
-async function getNodeTopology({ model, getValue, axios, storeGet, watchDependency }) {
-  watchDependency('model#/spec/kubeDB/deployment/default')
-  watchDependency('model#/spec/kubeDB/clusterTier/default')
+async function getNodeTopology({ axios, storeGet, commit }) {
   const owner = storeGet('/route/params/user')
   const cluster = storeGet('/route/params/cluster')
-  const deploymentType = getValue(model, '/spec/kubeDB/deployment/default') || ''
-  const clusterTier = getValue(model, '/spec/kubeDB/clusterTier/default') || ''
-  const nodeTopologyList = getValue(model, `/spec/kubeDB/clusterTier/nodeTopology/available`) || []
-  let mappedResp = []
 
-  if (!nodeTopologyApiCalled) {
-    console.log('')
-    try {
-      const url = `/clusters/${owner}/${cluster}/proxy/node.k8s.appscode.com/v1alpha1/nodetopologies`
-      const resp = await axios.get(url)
-      nodeTopologyListFromApi = resp.data?.items
-      nodeTopologyApiCalled = true
-      const filteredResp = resp.data?.items.filter(
-        (item) =>
-          item.metadata.labels?.['node.k8s.appscode.com/tenancy'] === deploymentType.toLowerCase(),
-      )
-      mappedResp = filteredResp?.map((item) => {
-        const name = (item.metadata && item.metadata.name) || ''
-        return name
-      })
-    } catch (e) {
-      console.log(e)
-    }
-  } else {
-    const filteredResp = nodeTopologyListFromApi.filter(
-      (item) =>
-        item.metadata.labels?.['node.k8s.appscode.com/tenancy'] === deploymentType.toLowerCase(),
-    )
-    mappedResp = filteredResp?.map((item) => {
+  try {
+    const url = `/clusters/${owner}/${cluster}/proxy/node.k8s.appscode.com/v1alpha1/nodetopologies`
+    const resp = await axios.get(url)
+    const nodeTopologyListFromApi = resp.data?.items
+
+    const mappedResp = nodeTopologyListFromApi?.map((item) => {
       const name = (item.metadata && item.metadata.name) || ''
       return name
     })
-  }
-
-  const statusUrl = `/clustersv2/${owner}/${cluster}/status`
-  if (provider.length === 0) {
-    try {
-      const resp = await axios.get(statusUrl)
-      provider = resp.data?.provider
-    } catch (e) {
-      console.log(e)
-    }
-  }
-
-  const filteredList = filterNodeTopology(nodeTopologyList, clusterTier, provider, mappedResp)
-  console.log(nodeTopologyList)
-  console.log(filteredList)
-  return filteredList
-}
-
-function filterNodeTopology(list, tier, provider, mappedList) {
-  // first filter the list from value that exists from the filtered list got from API
-  const filteredList = list.filter((item) => {
-    return mappedList.includes(item)
-  })
-
-  // filter the list based on clusterTier
-  if (provider === 'EKS') {
-    return filteredList.filter((item) => {
-      if (tier === 'CPUOptimized') return item.startsWith('c')
-      else if (tier === 'MemoryOptimized') return item.startsWith('r')
-      else return !item.startsWith('c') && !item.startsWith('r')
+    commit('wizard/model$update', {
+      path: '/spec/kubeDB/clusterTier/nodeTopology/available',
+      value: mappedResp,
+      force: true,
     })
-  } else if (provider === 'AKS') {
-    return filteredList.filter((item) => {
-      if (tier === 'CPUOptimized') return item.startsWith('f') || item.startsWith('fx')
-      else if (tier === 'MemoryOptimized')
-        return (
-          item.startsWith('e') ||
-          item.startsWith('eb') ||
-          item.startsWith('ec') ||
-          item.startsWith('m') ||
-          item.startsWith('d')
-        )
-      else
-        return (
-          !(item.startsWith('f') || item.startsWith('fx')) &&
-          !(
-            item.startsWith('e') ||
-            item.startsWith('eb') ||
-            item.startsWith('ec') ||
-            item.startsWith('m') ||
-            item.startsWith('d')
-          )
-        )
-    })
-  } else if (provider === 'GKE') {
-    return filteredList.filter((item) => {
-      if (tier === 'CPUOptimized')
-        return item.startsWith('h3') || item.startsWith('c2') || item.startsWith('c2d')
-      else if (tier === 'MemoryOptimized')
-        return (
-          item.startsWith('x4') ||
-          item.startsWith('m1') ||
-          item.startsWith('m2') ||
-          item.startsWith('m3')
-        )
-      else
-        return (
-          !(item.startsWith('h3') || item.startsWith('c2') || item.startsWith('c2d')) &&
-          !(
-            item.startsWith('x4') ||
-            item.startsWith('m1') ||
-            item.startsWith('m2') ||
-            item.startsWith('m3')
-          )
-        )
-    })
+    return mappedResp
+  } catch (e) {
+    console.log(e)
+    return []
   }
 }
 
 function isConfigureDb({ getValue, discriminator, watchDependency }, value) {
-  watchDependency(`discriminator#/${value}`)
-  const resp = getValue(discriminator, `/${value}`)
+  watchDependency(`discriminator#/${value}/isConfigure`)
+  const resp = getValue(discriminator, `/${value}/isConfigure`)
   return resp
 }
 
-const isApiCalled = {
-  clickhouse: false,
-  configureDruid: false,
-  configureElasticsearch: false,
-  configureFerretDB: false,
-  configureKafka: false,
-  configureMSSQLServer: false,
-  configureMariaDB: false,
-  configureMemcached: false,
-  configureMongoDB: false,
-  configureMySQL: false,
-  configurePerconaXtraDB: false,
-  configurePgBouncer: false,
-  configurePgpool: false,
-  configurePostgres: false,
-  configureProxySQL: false,
-  configureRabbitMQ: false,
-  configureRedis: false,
-  configureSinglestore: false,
-  configureSolr: false,
-  configureZooKeeper: false,
-}
-
-const apiData = {
-  clickhouse: [],
-  configureDruid: [],
-  configureElasticsearch: [],
-  configureFerretDB: [],
-  configureKafka: [],
-  configureMSSQLServer: [],
-  configureMariaDB: [],
-  configureMemcached: [],
-  configureMongoDB: [],
-  configureMySQL: [],
-  configurePerconaXtraDB: [],
-  configurePgBouncer: [],
-  configurePgpool: [],
-  configurePostgres: [],
-  configureProxySQL: [],
-  configureRabbitMQ: [],
-  configureRedis: [],
-  configureSinglestore: [],
-  configureSolr: [],
-  configureZooKeeper: [],
-}
-
-async function FetchDbVersions({ storeGet, axios }, db) {
+async function FetchDbVersions(
+  { storeGet, axios, getValue, setDiscriminatorValue, discriminator },
+  db,
+) {
   const owner = storeGet('/route/params/user')
   const cluster = storeGet('/route/params/cluster')
+  const isApiCalled = getValue(discriminator, `/${db}/isApiCalled`)
   const url = `/clusters/${owner}/${cluster}/proxy/catalog.kubedb.com/v1alpha1/${db}versions`
-  if (!isApiCalled[db]) {
+  let data = []
+  if (!isApiCalled) {
     const resp = await axios.get(url)
-    apiData[db] = resp.data.items.map((item) => {
+    data = resp.data.items.map((item) => {
       return {
         text: item.spec.version,
         value: item.spec.version,
       }
     })
-    isApiCalled[db] = true
+    setDiscriminatorValue(`/${db}/isApiCalled`, true)
+    setDiscriminatorValue(`/${db}/versions`, data)
+  } else {
+    data = getValue(discriminator, `/${db}/versions`)
   }
-  return apiData[db]
+  return data
 }
 
 function clearDefaultVersion({ commit }, db) {
-  console.log('changed')
   commit('wizard/model$update', {
     path: `/spec/kubeDB/databases/${db}/versions/default`,
     value: '',
@@ -202,11 +69,93 @@ function clearDefaultVersion({ commit }, db) {
 }
 
 function availableVersions({ getValue, model, watchDependency }, db) {
-  watchDependency(
-    `schema#/properties/spec/properties/kubeDB/properties/databases/properties/${db}/properties/versions/properties/available`,
-  )
-  console.log(getValue(model, `/spec/kubeDB/databases/${db}/versions/available`))
+  watchDependency(`model#/spec/kubeDB/databases/${db}/versions/available`)
   return getValue(model, `/spec/kubeDB/databases/${db}/versions/available`)
+}
+
+async function getPlacements({ axios, storeGet, commit }) {
+  const owner = storeGet('/route/params/user')
+  const cluster = storeGet('/route/params/cluster')
+  const url = `/clusters/${owner}/${cluster}/proxy/node.k8s.appscode.com/v1alpha1/nodetopologies`
+  try {
+    const resp = await axios.get(url)
+
+    const mappedResp = resp?.data?.items.map((item) => {
+      const name = (item.metadata && item.metadata.name) || ''
+      return name
+    })
+
+    return mappedResp
+  } catch (e) {
+    console.log(e)
+    return []
+  }
+}
+
+async function getStorageClass({ axios, storeGet, commit }) {
+  const owner = storeGet('/route/params/user')
+  const cluster = storeGet('/route/params/cluster')
+  const url = `/clusters/${owner}/${cluster}/proxy/storage.k8s.io/v1/storageclasses`
+  try {
+    const resp = await axios.get(url)
+    const mappedResp = resp?.data?.items.map((item) => {
+      const name = (item.metadata && item.metadata.name) || ''
+      return name
+    })
+
+    return mappedResp
+  } catch (e) {
+    console.log(e)
+    return []
+  }
+}
+
+async function getClusterIssuers({ axios, storeGet }) {
+  const owner = storeGet('/route/params/user')
+  const cluster = storeGet('/route/params/cluster')
+  const url = `/clusters/${owner}/${cluster}/proxy/cert-manager.io/v1/clusterissuers`
+  try {
+    const resp = await axios.get(url)
+    console.log(resp)
+    const mappedResp = resp?.data?.items.map((item) => {
+      const name = (item.metadata && item.metadata.name) || ''
+      return name
+    })
+
+    return mappedResp
+  } catch (e) {
+    console.log(e)
+    return []
+  }
+}
+
+async function getNamespaces({ axios, storeGet }) {
+  const params = storeGet('/route/params')
+  const { user, cluster, group, version, resource } = params
+  try {
+    const resp = await axios.post(
+      `/clusters/${user}/${cluster}/proxy/identity.k8s.appscode.com/v1alpha1/selfsubjectnamespaceaccessreviews`,
+      {
+        apiVersion: 'identity.k8s.appscode.com/v1alpha1',
+        kind: 'SelfSubjectNamespaceAccessReview',
+        spec: {
+          resourceAttributes: [
+            {
+              verb: 'create',
+              group: group,
+              version: version,
+              resource: resource,
+            },
+          ],
+        },
+      },
+    )
+    const namespaces = resp?.data?.status?.namespaces || []
+    return namespaces
+  } catch (e) {
+    console.log(e)
+    return []
+  }
 }
 
 return {
@@ -216,4 +165,8 @@ return {
   FetchDbVersions,
   availableVersions,
   clearDefaultVersion,
+  getPlacements,
+  getStorageClass,
+  getClusterIssuers,
+  getNamespaces,
 }
