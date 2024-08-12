30f6f7e28e4b4b7c7bae6972eb92cf5670b02d08
Arnob kumar saha <arnob@appscode.com>
2024-08-12 15:17:49 +0600

    Remove solr `replicaSet` field
    Signed-off-by: Arnob kumar saha <arnob@appscode.com>

--- apis/wizards/v1alpha1/kubedbcom_solr_editor_options_types.go
+++ apis/wizards/v1alpha1/kubedbcom_solr_editor_options_types.go
@@ -45,25 +45,22 @@ type KubedbcomSolrEditorOptionsSpecSpec struct {
 	// +optional
 	Annotations map[string]string `json:"annotations"`
 	// +optional
-	Labels         map[string]string `json:"labels"`
-	Mode           SolrMode          `json:"mode"`
-	ReplicaSet     SolrReplicaSet    `json:"replicaSet"`
-	Topology       SolrTopology      `json:"topology"`
-	ZookeeperRef   ObjectReference   `json:"zookeeperRef"`
-	Persistence    Persistence       `json:"persistence"`
-	PodResources   PodResources      `json:"podResources"`
-	AuthSecret     AuthSecret        `json:"authSecret"`
-	DeletionPolicy DeletionPolicy    `json:"deletionPolicy"`
-	Configuration  string            `json:"configuration"`
-	Admin          AdminOptions      `json:"admin"`
+	Labels map[string]string `json:"labels"`
+	Mode   SolrMode          `json:"mode"`
+	// +optional
+	Replicas       int             `json:"replicas"`
+	Topology       SolrTopology    `json:"topology"`
+	ZookeeperRef   ObjectReference `json:"zookeeperRef"`
+	Persistence    Persistence     `json:"persistence"`
+	PodResources   PodResources    `json:"podResources"`
+	AuthSecret     AuthSecret      `json:"authSecret"`
+	DeletionPolicy DeletionPolicy  `json:"deletionPolicy"`
+	Configuration  string          `json:"configuration"`
+	Admin          AdminOptions    `json:"admin"`
 }
 
 // +kubebuilder:validation:Enum=Standalone;Replicaset;Topology
 type SolrMode string
-
-type SolrReplicaSet struct {
-	Replicas int `json:"replicas"`
-}
 
 type SolrNode struct {
 	Replicas     int          `json:"replicas"`
