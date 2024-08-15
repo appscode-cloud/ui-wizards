73ce1eb00bba731d77822ebc784ee48d061fe50a
SamiulSourav <samiul@appscode.com>
2024-08-15 18:24:28 +0600

    finalize stash-presets
    Signed-off-by: SamiulSourav <samiul@appscode.com>

--- charts/uibytebuildersdev-component-stash-presets/ui/functions.js
+++ charts/uibytebuildersdev-component-stash-presets/ui/functions.js
@@ -1,5 +1,3 @@
-let storageSecretSwitch = false
-
 function providerType({ getValue, watchDependency, model }, value) {
   watchDependency('model#/backend/provider')
   const provider = getValue(model, '/backend/provider')
@@ -12,13 +10,13 @@ function isSwitchOn({ watchDependency, getValue, discriminator, model }, type) {
   return val
 }
 
-function setStorageSecret() {
-  return storageSecretSwitch
+function setStorageSecret({ getValue, model }) {
+  const secret = getValue(model, '/storageSecret/create')
+  return secret
 }
 
 function onAuthChange({ getValue, discriminator, commit }, type) {
   const auth = getValue(discriminator, `/${type}`) || false
-  storageSecretSwitch = auth
   commit('wizard/model$update', {
     path: '/storageSecret/create',
     value: auth,
@@ -34,6 +32,35 @@ function returnFalse() {
   return false
 }
 
+async function getNamespaces({ axios, storeGet }) {
+  const params = storeGet('/route/params')
+  const { user, cluster, group, version, resource } = params
+  try {
+    const resp = await axios.post(
+      `/clusters/${user}/${cluster}/proxy/identity.k8s.appscode.com/v1alpha1/selfsubjectnamespaceaccessreviews`,
+      {
+        apiVersion: 'identity.k8s.appscode.com/v1alpha1',
+        kind: 'SelfSubjectNamespaceAccessReview',
+        spec: {
+          resourceAttributes: [
+            {
+              verb: 'create',
+              group: group,
+              version: version,
+              resource: resource,
+            },
+          ],
+        },
+      },
+    )
+    const namespaces = resp?.data?.status?.namespaces || []
+    return namespaces
+  } catch (e) {
+    console.log(e)
+    return []
+  }
+}
+
 return {
   providerType,
   isSwitchOn,
@@ -41,4 +68,5 @@ return {
   onAuthChange,
   setProvider,
   returnFalse,
+  getNamespaces,
 }
