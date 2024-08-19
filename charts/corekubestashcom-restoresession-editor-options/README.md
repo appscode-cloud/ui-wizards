b54ffbab4ff9e3f0063416d34c183a2677364c30
Arnob Kumar Saha <arnob@appscode.com>
2024-08-19 18:09:15 +0600

    Implement kubestash restore (#551)
    Signed-off-by: Arnob kumar saha <arnob@appscode.com>

---
+++ charts/corekubestashcom-restoresession-editor-options/README.md
@@ -0,0 +1,83 @@
+# KubeStash RestoreSession Editor UI Options
+
+[KubeStash RestoreSession Editor UI Options](https://byte.builders) - KubeStash RestoreSession Editor UI Options
+
+## TL;DR;
+
+```bash
+$ helm repo add bytebuilders-ui https://bundles.byte.builders/ui/
+$ helm repo update
+$ helm search repo bytebuilders-ui/corekubestashcom-restoresession-editor-options --version=v0.4.21
+$ helm upgrade -i corekubestashcom-restoresession-editor-options bytebuilders-ui/corekubestashcom-restoresession-editor-options -n kube-system --create-namespace --version=v0.4.21
+```
+
+## Introduction
+
+This chart deploys a KubeStash RestoreSession Editor UI Options on a [Kubernetes](http://kubernetes.io) cluster using the [Helm](https://helm.sh) package manager.
+
+## Prerequisites
+
+- Kubernetes 1.14+
+
+## Installing the Chart
+
+To install/upgrade the chart with the release name `corekubestashcom-restoresession-editor-options`:
+
+```bash
+$ helm upgrade -i corekubestashcom-restoresession-editor-options bytebuilders-ui/corekubestashcom-restoresession-editor-options -n kube-system --create-namespace --version=v0.4.21
+```
+
+The command deploys a KubeStash RestoreSession Editor UI Options on the Kubernetes cluster in the default configuration. The [configuration](#configuration) section lists the parameters that can be configured during installation.
+
+> **Tip**: List all releases using `helm list`
+
+## Uninstalling the Chart
+
+To uninstall the `corekubestashcom-restoresession-editor-options`:
+
+```bash
+$ helm uninstall corekubestashcom-restoresession-editor-options -n kube-system
+```
+
+The command removes all the Kubernetes components associated with the chart and deletes the release.
+
+## Configuration
+
+The following table lists the configurable parameters of the `corekubestashcom-restoresession-editor-options` chart and their default values.
+
+|                 Parameter                  |                    Description                     |             Default             |
+|--------------------------------------------|----------------------------------------------------|---------------------------------|
+| metadata.resource.group                    |                                                    | <code>core.kubestash.com</code> |
+| metadata.resource.kind                     |                                                    | <code>RestoreSession</code>     |
+| metadata.resource.name                     |                                                    | <code>restoresessions</code>    |
+| metadata.resource.scope                    |                                                    | <code>Namespaced</code>         |
+| metadata.resource.version                  |                                                    | <code>v1alpha1</code>           |
+| metadata.release.name                      | Release name                                       | <code>""</code>                 |
+| metadata.release.namespace                 | Release namespace                                  | <code>""</code>                 |
+| spec.annotations                           | Annotations to add to the database custom resource | <code>{}</code>                 |
+| spec.labels                                | Labels to add to all the template objects          | <code>{}</code>                 |
+| spec.datasource.repository.name            |                                                    | <code>""</code>                 |
+| spec.datasource.repository.namespace       |                                                    | <code>""</code>                 |
+| spec.datasource.snapshot                   |                                                    | <code>latest</code>             |
+| spec.datasource.encryptionSecret.name      |                                                    | <code>""</code>                 |
+| spec.datasource.encryptionSecret.namespace |                                                    | <code>""</code>                 |
+| spec.addon.name                            |                                                    | <code>""</code>                 |
+| spec.addon.jobTemplate.securityContext     |                                                    | <code></code>                   |
+| spec.target.apiGroup                       |                                                    | <code>""</code>                 |
+| spec.target.kind                           |                                                    | <code>""</code>                 |
+| spec.target.namespace                      |                                                    | <code>""</code>                 |
+| spec.target.name                           |                                                    | <code>""</code>                 |
+
+
+Specify each parameter using the `--set key=value[,key=value]` argument to `helm upgrade -i`. For example:
+
+```bash
+$ helm upgrade -i corekubestashcom-restoresession-editor-options bytebuilders-ui/corekubestashcom-restoresession-editor-options -n kube-system --create-namespace --version=v0.4.21 --set metadata.resource.group=core.kubestash.com
+```
+
+Alternatively, a YAML file that specifies the values for the parameters can be provided while
+installing the chart. For example:
+
+```bash
+$ helm upgrade -i corekubestashcom-restoresession-editor-options bytebuilders-ui/corekubestashcom-restoresession-editor-options -n kube-system --create-namespace --version=v0.4.21 --values values.yaml
+```
---
+++ charts/corekubestashcom-restoresession-editor-options/ui/functions.js
@@ -0,0 +1,427 @@
+async function getResources(
+  { axios, storeGet, model, getValue, watchDependency },
+  group,
+  version,
+  resource,
+  namespaced,
+) {
+  const owner = storeGet('/route/params/user')
+  const cluster = storeGet('/route/params/cluster')
+  let namespace = ''
+  if (namespaced) {
+    namespace = getValue(model, '/metadata/release/namespace')
+    watchDependency('model#/metadata/release/namespace')
+  }
+
+  if (!namespaced || namespace) {
+    // call api if resource is either not namespaced
+    // or namespaced and user has selected a namespace
+    try {
+      const resp = await axios.get(
+        `/clusters/${owner}/${cluster}/proxy/${group}/${version}${
+          namespace ? '/namespaces/' + namespace : ''
+        }/${resource}`,
+        {
+          params: { filter: { items: { metadata: { name: null } } } },
+        },
+      )
+
+      const resources = (resp && resp.data && resp.data.items) || []
+
+      resources.map((item) => {
+        const name = (item.metadata && item.metadata.name) || ''
+        item.text = name
+        item.value = name
+        return true
+      })
+      return resources
+    } catch (e) {
+      console.log(e)
+      return []
+    }
+  } else return []
+}
+
+function hasOperationQuery(route) {
+  const { operation } = route.query
+  return !!operation
+}
+function showAndInitName({ route, commit }) {
+  const ver = hasOperationQuery(route)
+  if (ver) {
+    const { name } = route.query
+
+    commit('wizard/model$update', {
+      path: 'metadata/release/name',
+      value: `${name}-restore-${new Date().getTime()}`,
+      force: true,
+    })
+
+    return false
+  } else {
+    return true
+  }
+}
+function showAndInitNamespace({ route, commit }) {
+  const ver = hasOperationQuery(route)
+  if (ver) {
+    const { namespace } = route.query
+
+    commit('wizard/model$update', {
+      path: 'metadata/release/namespace',
+      value: namespace,
+      force: true,
+    })
+
+    return false
+  } else {
+    return true
+  }
+}
+function showIfDoesNotHaveOperationQuery({ route }) {
+  return !hasOperationQuery(route)
+}
+function initNamespace({ route }) {
+  const { namespace } = route.query || {}
+  return namespace || null
+}
+function isNamespaceDisabled({ route }) {
+  return !!initNamespace({ route }) || !isVariantAvailable
+}
+function isDatabaseSelectDisabled({ route }) {
+  const { database } = route.query || {}
+  return !!database
+}
+
+function labelsDisabilityChecker({ itemCtx }) {
+  const { key } = itemCtx
+  if (key.startsWith('app.kubernetes.io') || key.includes('helm')) return true
+  else return false
+}
+
+async function fetchJsons({ axios, itemCtx, setDiscriminatorValue }, discriminatorPath) {
+  let ui = {}
+  let language = {}
+  let functions = {}
+  const { name, sourceRef, version, packageviewUrlPrefix } = itemCtx.chart
+  try {
+    ui = await axios.get(
+      `${packageviewUrlPrefix}/create-ui.yaml?name=${name}&sourceApiGroup=${sourceRef.apiGroup}&sourceKind=${sourceRef.kind}&sourceNamespace=${sourceRef.namespace}&sourceName=${sourceRef.name}&version=${version}&format=json`,
+    )
+    language = await axios.get(
+      `${packageviewUrlPrefix}/language.yaml?name=${name}&sourceApiGroup=${sourceRef.apiGroup}&sourceKind=${sourceRef.kind}&sourceNamespace=${sourceRef.namespace}&sourceName=${sourceRef.name}&version=${version}&format=json`,
+    )
+    const functionString = await axios.get(
+      `${packageviewUrlPrefix}/functions.js?name=${name}&sourceApiGroup=${sourceRef.apiGroup}&sourceKind=${sourceRef.kind}&sourceNamespace=${sourceRef.namespace}&sourceName=${sourceRef.name}&version=${version}`,
+    )
+    // declare evaluate the functionString to get the functions Object
+    const evalFunc = new Function(functionString.data || '')
+    functions = evalFunc()
+  } catch (e) {
+    console.log(e)
+  }
+
+  if (discriminatorPath) {
+    setDiscriminatorValue(discriminatorPath, {
+      ui: ui.data || {},
+      language: language.data || {},
+      functions,
+    })
+  }
+
+  return {
+    ui: ui.data || {},
+    language: language.data || {},
+    functions,
+  }
+}
+
+let databaseToTypeMap = {}
+
+async function fetchDatabases({ axios, storeGet, model, getValue, watchDependency, commit }) {
+  const owner = storeGet('/route/params/user')
+  const name = storeGet('/route/query/name')
+  const cluster = storeGet('/route/params/cluster')
+  const namespace = getValue(model, '/metadata/release/namespace')
+  watchDependency('model#/metadata/release/namespace')
+
+  if (namespace) {
+    // call api if user has selected a namespace
+    try {
+      const resp = await axios.get(
+        `/clusters/${owner}/${cluster}/proxy/appcatalog.appscode.com/v1alpha1/namespaces/${namespace}/appbindings`,
+        {
+          params: {
+            filter: {
+              items: {
+                apiVersion: null,
+                kind: null,
+                metadata: { name: null, namespace: null },
+                spec: { type: null, appRef: { apiGroup: null } },
+              },
+            },
+          },
+        },
+      )
+
+      const resources = (resp && resp.data && resp.data.items) || []
+
+      const mappedResources = resources
+        .filter((item) => {
+          const appRef = item.spec?.appRef?.apiGroup || ''
+          return appRef === 'kubedb.com' || appRef === 'kubevault.com'
+        })
+        .map((item) => {
+          const apiVersion = item.apiVersion || ''
+          const kind = item.kind || ''
+          const name = (item.metadata && item.metadata.name) || ''
+          const namespace = (item.metadata && item.metadata.namespace) || ''
+          return {
+            text: name,
+            value: {
+              apiVersion,
+              kind,
+              name,
+              namespace,
+            },
+          }
+        })
+
+      // update database to type map
+      databaseToTypeMap = {}
+      resources.forEach((rs) => {
+        const type = (rs.spec && rs.spec.type) || ''
+        const name = (rs.metadata && rs.metadata.name) || ''
+        if (type.startsWith('kubedb.com')) {
+          databaseToTypeMap[name] = type
+        }
+      })
+
+      if (name) {
+        // if database name is provided in route query
+        // find the option matching with database
+        const matchedOption = mappedResources.find((rs) => rs.text === name)
+        if (matchedOption) {
+          // set this value as target
+          commit('wizard/model$update', {
+            path: '/spec/target',
+            value: matchedOption.value,
+            force: true,
+          })
+        }
+      }
+
+      return mappedResources
+    } catch (e) {
+      console.log(e)
+      return []
+    }
+  } else return []
+}
+
+function showAndInitDatabase({ model, getValue, watchDependency, route }) {
+  // watch targetRef (database)
+  // if provided in query, then it will be set by fetchDatabases
+  const target = getValue(model, '/spec/target')
+  watchDependency('model#/spec/target')
+
+  const { operation } = route.query
+  if (target.name && operation) {
+    return false
+  } else return true
+}
+
+async function fetchRepositories({ axios, storeGet, model, getValue, watchDependency }) {
+  const owner = storeGet('/route/params/user')
+  const cluster = storeGet('/route/params/cluster')
+  const namespace = getValue(model, '/metadata/release/namespace')
+  watchDependency('model#/metadata/release/namespace')
+
+  if (namespace) {
+    // call api if user has selected a namespace
+    try {
+      const resp = await axios.get(
+        `/clusters/${owner}/${cluster}/proxy/core.kubestash.com/v1alpha1/namespaces/${namespace}/repositories`,
+        {
+          params: {
+            filter: {
+              items: {
+                metadata: { name: null, namespace: null },
+              },
+            },
+          },
+        },
+      )
+
+      const resources = (resp && resp.data && resp.data.items) || []
+
+      const mappedResources = resources.map((item) => {
+        const name = (item.metadata && item.metadata.name) || ''
+        const namespace = (item.metadata && item.metadata.namespace) || ''
+        return {
+          text: name,
+          value: {
+            name,
+            namespace,
+          },
+        }
+      })
+
+      return mappedResources
+    } catch (e) {
+      console.log(e)
+      return []
+    }
+  } else return []
+}
+
+function valueExists(value, getValue, path) {
+  const val = getValue(value, path)
+  if (val) return true
+  else return false
+}
+
+function showInterimVolumneTemplate({ model, getValue, watchDependency, commit }) {
+  const appbindingName = getValue(model, '/spec/target/name')
+  watchDependency('model#/spec/target/name')
+
+  let verdict = false
+  if (appbindingName) {
+    // find app binding type
+    const type = databaseToTypeMap[appbindingName]
+    if (type === 'kubedb.com/elasticsearch') verdict = true
+  }
+
+  if (!verdict) {
+    // delete interimVolumeTempalte if it exists
+    if (valueExists(model, getValue, '/spec/interimVolumeTemplate')) {
+      commit('wizard/model$delete', '/spec/interimVolumeTemplate')
+    }
+  }
+
+  return verdict
+}
+
+async function getStorageClassNames(
+  { axios, storeGet, commit, setDiscriminatorValue, getValue, model },
+  path,
+) {
+  const owner = storeGet('/route/params/user')
+  const cluster = storeGet('/route/params/cluster')
+
+  const resp = await axios.get(
+    `/clusters/${owner}/${cluster}/proxy/storage.k8s.io/v1/storageclasses`,
+    {
+      params: {
+        filter: { items: { metadata: { name: null, annotations: null } } },
+      },
+    },
+  )
+
+  const resources = (resp && resp.data && resp.data.items) || []
+
+  resources.map((item) => {
+    const name = (item.metadata && item.metadata.name) || ''
+    const isDefault =
+      item.metadata &&
+      item.metadata.annotations &&
+      item.metadata.annotations['storageclass.kubernetes.io/is-default-class']
+
+    if (isDefault && path) {
+      const className = getValue(model, path)
+      if (!className) {
+        commit('wizard/model$update', {
+          path: path,
+          value: name,
+          force: true,
+        })
+      }
+    }
+
+    item.text = name
+    item.value = name
+    return true
+  })
+
+  if (!path) {
+    setDiscriminatorValue('/storageClasses', resources)
+  }
+
+  return resources
+}
+
+const restoreSessionInitRunTimeSettings = {
+  pod: {
+    serviceAccountName: '',
+    securityContext: {
+      fsGroup: null,
+      runAsUser: null,
+      runAsGroup: null,
+    },
+  },
+}
+function showRuntimeSettingsForm({ discriminator, getValue, watchDependency, commit, model }) {
+  const customizeRestoreJobRuntimeSettings = getValue(
+    discriminator,
+    '/customizeRestoreJobRuntimeSettings',
+  )
+  watchDependency('discriminator#/customizeRestoreJobRuntimeSettings')
+
+  if (customizeRestoreJobRuntimeSettings) {
+    // set the runtime settings values with default values
+    if (!valueExists(model, getValue, '/spec/runtimeSettings')) {
+      // set new value
+      commit('wizard/model$update', {
+        path: '/spec/runtimeSettings',
+        value: restoreSessionInitRunTimeSettings,
+      })
+    }
+  } else {
+    commit('wizard/model$delete', '/spec/runtimeSettings')
+  }
+  return !!customizeRestoreJobRuntimeSettings
+}
+
+function getCreateNameSpaceUrl({ model, getValue, storeGet }) {
+  const user = storeGet('/route/params/user')
+  const cluster = storeGet('/route/params/cluster')
+
+  const domain = storeGet('/domain') || ''
+  if (domain.includes('bb.test')) {
+    return `http://console.bb.test:5990/${user}/kubernetes/${cluster}/core/v1/namespaces/create`
+  } else {
+    const editedDomain = domain.replace('kubedb', 'console')
+    return `${editedDomain}/${user}/kubernetes/${cluster}/core/v1/namespaces/create`
+  }
+}
+
+function isVariantAvailable({ storeGet }) {
+  const variant = storeGet('/route/query/variant')
+  return variant ? true : false
+}
+
+return {
+  isVariantAvailable,
+  getResources,
+
+  showAndInitName,
+  showAndInitNamespace,
+  showIfDoesNotHaveOperationQuery,
+
+  initNamespace,
+  isNamespaceDisabled,
+  isDatabaseSelectDisabled,
+  labelsDisabilityChecker,
+  fetchJsons,
+
+  fetchDatabases,
+  showAndInitDatabase,
+
+  fetchRepositories,
+
+  showInterimVolumneTemplate,
+  getStorageClassNames,
+
+  showRuntimeSettingsForm,
+  getCreateNameSpaceUrl,
+}
