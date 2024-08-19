e3dc8c5f4db922203256e0fe468f4604f5a0de97
Nihal Azmain <nihal@appscode.com>
2024-08-19 13:08:34 +0600

    Add reusable in featuresets
    Signed-off-by: Nihal Azmain <nihal@appscode.com>

--- charts/uibytebuildersdev-component-kubedb-ui-presets/ui/functions.js
+++ charts/uibytebuildersdev-component-kubedb-ui-presets/ui/functions.js
@@ -1,3 +1,125 @@
+function getOptions({ getValue, model, watchDependency }, type) {
+  watchDependency(`model#/${type}/available`)
+  const options = getValue(model, `/${type}/available`)
+  return options
+}
+
+async function getNodeTopology({ axios, storeGet, commit }) {
+  const owner = storeGet('/route/params/user')
+  const cluster = storeGet('/route/params/cluster')
+
+  try {
+    const url = `/clusters/${owner}/${cluster}/proxy/node.k8s.appscode.com/v1alpha1/nodetopologies`
+    const resp = await axios.get(url)
+    const nodeTopologyListFromApi = resp.data?.items
+
+    const mappedResp = nodeTopologyListFromApi?.map((item) => {
+      const name = (item.metadata && item.metadata.name) || ''
+      return name
+    })
+    commit('wizard/model$update', {
+      path: '/spec/kubeDB/clusterTier/nodeTopology/available',
+      value: mappedResp,
+      force: true,
+    })
+    return mappedResp
+  } catch (e) {
+    console.log(e)
+    return []
+  }
+}
+
+async function FetchAllDbVersions({ storeGet, axios, setDiscriminatorValue }) {
+  const owner = storeGet('/route/params/user')
+  const cluster = storeGet('/route/params/cluster')
+  const url = `/clusters/${owner}/${cluster}/proxy/catalog.kubedb.com/v1alpha1/all-available`
+
+  const resp = await axios.get(url)
+
+  setDiscriminatorValue(`/allDbVersions`, resp.data)
+  return resp.data
+}
+
+function isConfigureDb({ getValue, discriminator, watchDependency }, value) {
+  watchDependency(`discriminator#/${value}/isConfigure`)
+  const resp = getValue(discriminator, `/${value}/isConfigure`)
+  return resp
+}
+
+async function FetchDbVersions({ watchDependency, getValue, discriminator }, db) {
+  watchDependency(`discriminator#/allDbVersions`)
+  data = getValue(discriminator, `allDbVersions/${db}Version`)
+  return data
+}
+
+function clearDefaultVersion({ commit }, db) {
+  commit('wizard/model$update', {
+    path: `/databases/${db}/versions/default`,
+    value: '',
+    force: true,
+  })
+}
+
+function availableVersions({ getValue, model, watchDependency }, db) {
+  watchDependency(`model#/databases/${db}/versions/available`)
+  return getValue(model, `/databases/${db}/versions/available`)
+}
+
+async function getPlacements({ axios, storeGet }) {
+  const owner = storeGet('/route/params/user')
+  const cluster = storeGet('/route/params/cluster')
+  const url = `/clusters/${owner}/${cluster}/proxy/apps.k8s.appscode.com/v1/placementpolicies`
+  try {
+    const resp = await axios.get(url)
+
+    const mappedResp = resp?.data?.items.map((item) => {
+      const name = (item.metadata && item.metadata.name) || ''
+      return name
+    })
+
+    return mappedResp
+  } catch (e) {
+    console.log(e)
+    return []
+  }
+}
+
+async function getStorageClass({ axios, storeGet }) {
+  const owner = storeGet('/route/params/user')
+  const cluster = storeGet('/route/params/cluster')
+  const url = `/clusters/${owner}/${cluster}/proxy/storage.k8s.io/v1/storageclasses`
+  try {
+    const resp = await axios.get(url)
+    const mappedResp = resp?.data?.items.map((item) => {
+      const name = (item.metadata && item.metadata.name) || ''
+      return name
+    })
+
+    return mappedResp
+  } catch (e) {
+    console.log(e)
+    return []
+  }
+}
+
+async function getClusterIssuers({ axios, storeGet }) {
+  const owner = storeGet('/route/params/user')
+  const cluster = storeGet('/route/params/cluster')
+  const url = `/clusters/${owner}/${cluster}/proxy/cert-manager.io/v1/clusterissuers`
+  try {
+    const resp = await axios.get(url)
+    const mappedResp = resp?.data?.items.map((item) => {
+      const name = (item.metadata && item.metadata.name) || ''
+      return name
+    })
+
+    return mappedResp
+  } catch (e) {
+    console.log(e)
+    return []
+  }
+}
+
 async function getNamespaces({ axios, storeGet }) {
   const params = storeGet('/route/params')
   const { user, cluster, group, version, resource } = params
@@ -27,35 +149,84 @@ async function getNamespaces({ axios, storeGet }) {
   }
 }
 
-async function fetchNames({ getValue, axios, storeGet, watchDependency, model }, type) {
-  watchDependency(`model#/${type}/namespace`)
-  const username = storeGet('/route/params/user')
-  const clusterName = storeGet('/route/params/cluster')
-  const namespace = getValue(model, `/${type}/namespace`)
-  const suffix =
-    type === 'encryptionSecret'
-      ? 'secrets'
-      : type === 'retentionPolicy'
-      ? 'retentionpolicies'
-      : 'backupstorages'
-  const core = suffix === 'secrets' ? 'core' : 'storage.kubestash.com'
-  const version = suffix === 'secrets' ? 'v1' : 'v1alpha1'
-  const url = `http://bb.test:3003/api/v1/clusters/${username}/${clusterName}/proxy/${core}/${version}/namespaces/${namespace}/${suffix}`
+function setTool({ commit }) {
+  commit('wizard/model$update', {
+    path: '/spec/backup/tool',
+    value: 'KubeStash',
+    force: true,
+  })
+  return 'KubeStash'
+}
+
+function returnFalse() {
+  return false
+}
+
+async function fetchJsons({ axios, itemCtx }) {
+  let ui = {}
+  let language = {}
+  let functions = {}
+  const { name, sourceRef, version, packageviewUrlPrefix } = itemCtx.chart
+
   try {
-    if (namespace) {
-      const resp = await axios.get(url)
-      let names = resp?.data?.items
-      names = names.map((ele) => ele?.metadata?.name)
-      return names
-    }
+    ui = await axios.get(
+      `${packageviewUrlPrefix}/create-ui.yaml?name=${name}&sourceApiGroup=${sourceRef.apiGroup}&sourceKind=${sourceRef.kind}&sourceNamespace=${sourceRef.namespace}&sourceName=${sourceRef.name}&version=${version}&format=json`,
+    )
+    language = await axios.get(
+      `${packageviewUrlPrefix}/language.yaml?name=${name}&sourceApiGroup=${sourceRef.apiGroup}&sourceKind=${sourceRef.kind}&sourceNamespace=${sourceRef.namespace}&sourceName=${sourceRef.name}&version=${version}&format=json`,
+    )
+    const functionString = await axios.get(
+      `${packageviewUrlPrefix}/functions.js?name=${name}&sourceApiGroup=${sourceRef.apiGroup}&sourceKind=${sourceRef.kind}&sourceNamespace=${sourceRef.namespace}&sourceName=${sourceRef.name}&version=${version}`,
+    )
+    // declare evaluate the functionString to get the functions Object
+    const evalFunc = new Function(functionString.data || '')
+    functions = evalFunc()
   } catch (e) {
     console.log(e)
-    return []
   }
-  return []
+
+  return {
+    ui: ui.data || {},
+    language: language.data || {},
+    functions,
+  }
+}
+
+function presetNameEqualsTo({ storeGet }, value) {
+  const presetName = storeGet('/route/params/presetName') || ''
+  return presetName === value
+}
+
+async function fetchBackup({ storeGet, axios, commit }) {
+  const owner = storeGet('/route/params/user')
+  const cluster = storeGet('/route/params/cluster')
+  const url = `/clusters/${owner}/${cluster}/proxy/charts.x-helm.dev/v1alpha1/clusterchartpresets/stash-presets`
+
+  const resp = await axios.get(url)
+  console.log(resp.data)
+
+  commit('wizard/model$update', {
+    path: '/spec/kubeDB/backup',
+    value: resp.data.spec.values.spec.backup,
+    force: true,
+  })
+
+  return resp.data.spec.values.spec.backup
 }
 
 return {
+  getOptions,
+  getNodeTopology,
+  FetchAllDbVersions,
+  isConfigureDb,
+  FetchDbVersions,
+  availableVersions,
+  clearDefaultVersion,
+  getPlacements,
+  getStorageClass,
+  getClusterIssuers,
   getNamespaces,
-  fetchNames,
+  returnFalse,
+  presetNameEqualsTo,
+  fetchBackup,
 }
