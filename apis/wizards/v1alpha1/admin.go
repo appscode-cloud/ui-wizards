079f9a3c5b0d56492107e30768fa3ec36421c201
Tamal Saha <tamal@appscode.com>
2024-08-22 03:57:15 -0700

    Revise backup and monitoring options for kubedb ui
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/wizards/v1alpha1/admin.go
+++ apis/wizards/v1alpha1/admin.go
@@ -131,6 +131,12 @@ type MonitoringOperator struct {
 }
 
 type Monitoring struct {
+	Agent          mona.AgentType             `json:"agent"`
+	Exporter       PrometheusExporter         `json:"exporter"`
+	ServiceMonitor *mona.ServiceMonitorLabels `json:"serviceMonitor"`
+}
+
+type Monitoring struct {
 	Agent    mona.AgentType     `json:"agent"`
 	Exporter PrometheusExporter `json:"exporter"`
 	Toggle   bool               `json:"toggle"`
