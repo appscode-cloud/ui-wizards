3054ce62f9d65ded6cb8b22a3d3c65c858c23421
Al Asad Nur Riyad <asadnurriyad@appscode.com>
2024-08-27 10:12:56 +0600

    Update isKubedbPresent enable function (#585)
    * Fix typo
    
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>
    
    * Update kubedbPreset logic
    
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>
    
    ---------
    
    Signed-off-by: nurRiyad <asadnurriyad@gmail.com>

--- charts/uik8sappscodecom-featureset-opscenter-datastore-editor/ui/functions.js
+++ charts/uik8sappscodecom-featureset-opscenter-datastore-editor/ui/functions.js
@@ -494,12 +494,20 @@ function typeConvert(commit, enabledTypes, model, getValue) {
   return convertFromArray
 }
 
-function isKubedbUiPreset({ getValue, watchDependency, discriminator }) {
-  const enabledFeatures = getValue(discriminator, '/enabledFeatures') || []
+function isKubedbUiPreset({ getValue, storeGet, watchDependency, discriminator }) {
   watchDependency('discriminator#/enabledFeatures')
-  if (enabledFeatures?.includes('kubedb-ui-presets')) {
+  const enabledFeatures = getValue(discriminator, '/enabledFeatures') || []
+  const featureDetails = getFeatureDetails(storeGet, 'kubedb-ui-presets')
+  const isManaged = featureDetails?.status?.managed || false
+  const isPresetEnabled = enabledFeatures?.includes('kubedb-ui-presets') || false
+
+  if (isPresetEnabled && !isManaged) {
+    return false
+  } else if (isPresetEnabled) {
     return true
-  } else return false
+  } else {
+    return false
+  }
 }
 
 function getOptions({ getValue, model, watchDependency }, type) {
