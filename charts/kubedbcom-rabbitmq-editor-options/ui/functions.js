c784911c7987a881164de9aca1195fa14724b707
Sayed Tashinoor Rahman <75539185+SayedTahsin@users.noreply.github.com>
2024-09-13 19:43:11 +0600

    disabled deployment mode when nodetopo is empty (#608)
    Signed-off-by: SayedTahsin <mail.tahsin99@gmail.com>

--- charts/kubedbcom-rabbitmq-editor-options/ui/functions.js
+++ charts/kubedbcom-rabbitmq-editor-options/ui/functions.js
@@ -789,6 +789,19 @@ function fetchOptions({ model, getValue }, type) {
   return []
 }
 
+function isDeploymentModeDisable({ commit, getValue, model, watchDependency }) {
+  watchDependency('discriminator#/bundleApiLoaded')
+  const deploymentType = getValue(model, '/spec/admin/deployment/default')
+  if (!nodetopologiesDedicated.length && deploymentType === 'Dedicated') {
+    commit('wizard/model$update', {
+      path: '/spec/admin/deployment/default',
+      value: 'Shared',
+      force: true,
+    })
+  }
+  return !nodetopologiesDedicated.length
+}
+
 function getAdminOptions({ getValue, model, watchDependency }, type) {
   watchDependency('discriminator#/bundleApiLoaded')
 
@@ -1061,6 +1074,7 @@ function showAdditionalSettings({ watchDependency }) {
 }
 
 return {
+  isDeploymentModeDisable,
   initBundle,
   getNamespaces,
   updateAlertValue,
--- charts/kubedbcom-zookeeper-editor-options/ui/functions.js
+++ charts/kubedbcom-zookeeper-editor-options/ui/functions.js
@@ -762,6 +762,19 @@ function fetchOptions({ model, getValue }, type) {
   return []
 }
 
+function isDeploymentModeDisable({ commit, getValue, model, watchDependency }) {
+  watchDependency('discriminator#/bundleApiLoaded')
+  const deploymentType = getValue(model, '/spec/admin/deployment/default')
+  if (!nodetopologiesDedicated.length && deploymentType === 'Dedicated') {
+    commit('wizard/model$update', {
+      path: '/spec/admin/deployment/default',
+      value: 'Shared',
+      force: true,
+    })
+  }
+  return !nodetopologiesDedicated.length
+}
+
 function getAdminOptions({ getValue, model, watchDependency }, type) {
   watchDependency('discriminator#/bundleApiLoaded')
 
@@ -989,6 +1002,7 @@ function showAdditionalSettings({ watchDependency }) {
 }
 
 return {
+  isDeploymentModeDisable,
   initBundle,
   returnFalse,
   isVariantAvailable,
