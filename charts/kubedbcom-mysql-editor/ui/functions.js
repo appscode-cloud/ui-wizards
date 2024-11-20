9e26b39d48f608b9ac18936f2d1618ab0c8fc2d5
SayedTahsin <tahsin@appscode.com>
2024-11-20 12:42:02 +0600

    mysql completed
    Signed-off-by: SayedTahsin <tahsin@appscode.com>

--- charts/kubedbcom-mysql-editor/ui/functions.js
+++ charts/kubedbcom-mysql-editor/ui/functions.js
@@ -1972,7 +1972,223 @@ function getDefaultSchedule(
   const session = getValue(model, modelPath)
   return session[0].scheduler.schedule
 }
+
+//////////////////// Auto Scaler /////////////////
+let autoscaleType = ''
+let dbDetails = {}
+
+function isConsole({ storeGet, commit }) {
+  const isKube = isKubedb({ storeGet })
+
+  if (isKube) {
+    const dbName = storeGet('/route/query/name') || ''
+    commit('wizard/model$update', {
+      path: '/resources/autoscalingKubedbComMySQLAutoscaler/spec/databaseRef/name',
+      value: dbName,
+      force: true,
+    })
+    const operation = storeGet('/route/query/operation') || ''
+    if (operation.length) {
+      const splitOp = operation.split('-')
+      if (splitOp.length > 2) autoscaleType = splitOp[2]
+    }
+    const date = Math.floor(Date.now() / 1000)
+    const modifiedName = `${dbName}-${date}-autoscaling-${autoscaleType}`
+    commit('wizard/model$update', {
+      path: '/resources/autoscalingKubedbComMySQLAutoscaler/metadata/name',
+      value: modifiedName,
+      force: true,
+    })
+    const namespace = storeGet('/route/query/namespace') || ''
+    if (namespace) {
+      commit('wizard/model$update', {
+        path: '/resources/autoscalingKubedbComMySQLAutoscaler/metadata/namespace',
+        value: namespace,
+        force: true,
+      })
+    }
+  }
+
+  return !isKube
+}
+
+function isKubedb({ storeGet }) {
+  return !!storeGet('/route/query/operation')
+}
+
+function showOpsRequestOptions({ model, getValue, watchDependency, storeGet, discriminator }) {
+  if (isKubedb({ storeGet }) === true) return true
+  watchDependency('model#/resources/autoscalingKubedbComMySQLAutoscaler/spec/databaseRef/name')
+  watchDependency('discriminator#/autoscalingType')
+  return (
+    !!getValue(model, '/resources/autoscalingKubedbComMySQLAutoscaler/spec/databaseRef/name') &&
+    !!getValue(discriminator, '/autoscalingType')
+  )
+}
+
+async function getNamespaces({ axios, storeGet }) {
+  const owner = storeGet('/route/params/user')
+  const cluster = storeGet('/route/params/cluster')
+
+  const resp = await axios.get(`/clusters/${owner}/${cluster}/proxy/core/v1/namespaces`, {
+    params: { filter: { items: { metadata: { name: null } } } },
+  })
+
+  const resources = (resp && resp.data && resp.data.items) || []
+
+  return resources.map((item) => {
+    const name = (item.metadata && item.metadata.name) || ''
+    return {
+      text: name,
+      value: name,
+    }
+  })
+}
+
+async function getMysqlDbs({ axios, storeGet, model, getValue, watchDependency }) {
+  watchDependency('model#/resources/autoscalingKubedbComMySQLAutoscaler/metadata/namespace')
+  const namespace = getValue(
+    model,
+    '/resources/autoscalingKubedbComMySQLAutoscaler/metadata/namespace',
+  )
+  const owner = storeGet('/route/params/user')
+  const cluster = storeGet('/route/params/cluster')
+
+  const resp = await axios.get(
+    `/clusters/${owner}/${cluster}/proxy/kubedb.com/v1alpha2/namespaces/${namespace}/mysqls`,
+    {
+      params: { filter: { items: { metadata: { name: null } } } },
+    },
+  )
+
+  const resources = (resp && resp.data && resp.data.items) || []
+
+  return resources.map((item) => {
+    const name = (item.metadata && item.metadata.name) || ''
+    return {
+      text: name,
+      value: name,
+    }
+  })
+}
+
+function initMetadata({ getValue, discriminator, model, commit, storeGet }) {
+  const dbName =
+    getValue(model, '/resources/autoscalingKubedbComMySQLAutoscaler/spec/databaseRef/name') || ''
+  const type = getValue(discriminator, '/autoscalingType') || ''
+  const date = Math.floor(Date.now() / 1000)
+  const resource = storeGet('/route/params/resource')
+  const scalingName = dbName ? dbName : resource
+  const modifiedName = `${scalingName}-${date}-autoscaling-${type ? type : ''}`
+  if (modifiedName)
+    commit('wizard/model$update', {
+      path: '/resources/autoscalingKubedbComMySQLAutoscaler/metadata/name',
+      value: modifiedName,
+      force: true,
+    })
+
+  // delete the other type object from vuex wizard model
+  if (type === 'compute')
+    commit('wizard/model$delete', '/resources/autoscalingKubedbComMySQLAutoscaler/spec/storage')
+  if (type === 'storage')
+    commit('wizard/model$delete', '/resources/autoscalingKubedbComMySQLAutoscaler/spec/compute')
+}
+
+function onNamespaceChange({ model, getValue, commit }) {
+  const namespace = getValue(
+    model,
+    '/resources/autoscalingKubedbComMySQLAutoscaler/metadata/namespace',
+  )
+  if (!namespace) {
+    commit(
+      'wizard/model$delete',
+      '/resources/autoscalingKubedbComMySQLAutoscaler/spec/databaseRef/name',
+    )
+  }
+}
+
+function ifScalingTypeEqualsTo(
+  { storeGet, watchDependency, getValue, discriminator, model },
+  type,
+) {
+  watchDependency('discriminator#/autoscalingType')
+  watchDependency('model#/resources/autoscalingKubedbComMySQLAutoscaler/spec/databaseRef/name')
+
+  const operation = storeGet('/route/query/operation') || ''
+  if (operation.length) {
+    const splitOp = operation.split('-')
+    if (splitOp.length > 2) autoscaleType = splitOp[2]
+  } else autoscaleType = getValue(discriminator, '/autoscalingType') || ''
+  const isDatabaseSelected = !!getValue(
+    model,
+    '/resources/autoscalingKubedbComMySQLAutoscaler/spec/databaseRef/name',
+  )
+  return autoscaleType === type && isDatabaseSelected
+}
+
+async function fetchNodeTopology({ axios, storeGet }) {
+  const owner = storeGet('/route/params/user') || ''
+  const cluster = storeGet('/route/params/cluster') || ''
+  const url = `/clusters/${owner}/${cluster}/proxy/node.k8s.appscode.com/v1alpha1/nodetopologies`
+  try {
+    const resp = await axios.get(url)
+    const list = (resp && resp.data?.items) || []
+    const mappedList = list.map((item) => {
+      const name = (item.metadata && item.metadata.name) || ''
+      return name
+    })
+    return mappedList
+  } catch (e) {
+    console.log(e)
+  }
+  return []
+}
+
+function isNodeTopologySelected({ watchDependency, model, getValue }) {
+  watchDependency(
+    'model#/resources/autoscalingKubedbComMySQLAutoscaler/spec/compute/nodeTopology/name',
+  )
+  const nodeTopologyName =
+    getValue(
+      model,
+      '/resources/autoscalingKubedbComMySQLAutoscaler/spec/compute/nodeTopology/name',
+    ) || ''
+  return !!nodeTopologyName.length
+}
+
+function setControlledResources({ commit }, type) {
+  const list = ['cpu', 'memory']
+  const path = `/resources/autoscalingKubedbComMySQLAutoscaler/spec/compute/${type}/controlledResources`
+  commit('wizard/model$update', {
+    path: path,
+    value: list,
+    force: true,
+  })
+  return list
+}
+
+function setTrigger() {
+  return 'On'
+}
+
+function setApplyToIfReady() {
+  return 'IfReady'
+}
+
 return {
+  isConsole,
+  getNamespaces,
+  getMysqlDbs,
+  isKubedb,
+  initMetadata,
+  onNamespaceChange,
+  ifScalingTypeEqualsTo,
+  fetchNodeTopology,
+  isNodeTopologySelected,
+  setControlledResources,
+  setTrigger,
+  setApplyToIfReady,
+  showOpsRequestOptions,
   setInitSchedule,
   fetchNames,
   fetchNamespaces,
