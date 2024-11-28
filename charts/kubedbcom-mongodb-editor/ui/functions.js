dc77513b766fef3729c6d5dba80379cd7e3d7620
SayedTahsin <tahsin@appscode.com>
2024-11-28 17:58:15 +0600

    updated function.js
    Signed-off-by: SayedTahsin <tahsin@appscode.com>

--- charts/kubedbcom-mongodb-editor/ui/functions.js
+++ charts/kubedbcom-mongodb-editor/ui/functions.js
@@ -2628,7 +2628,41 @@ function setMetadata({ storeGet, mode, commit }) {
   }
 }
 
+function handleUnit({ commit, model, getValue }, path, type = 'bound') {
+  let value = getValue(model, `/resources/${path}`)
+  if (type === 'scalingRules') {
+    const updatedValue = []
+    value.forEach((ele) => {
+      let appliesUpto = ele['appliesUpto']
+      let threshold = ele['threshold']
+      if (appliesUpto && !isNaN(appliesUpto)) {
+        appliesUpto += 'Gi'
+      }
+      if (!isNaN(threshold)) {
+        threshold += 'pc'
+      }
+      updatedValue.push({ threshold, appliesUpto })
+    })
+    if (JSON.stringify(updatedValue) !== JSON.stringify(value)) {
+      commit('wizard/model$update', {
+        path: `/resources/${path}`,
+        value: updatedValue,
+        force: true,
+      })
+    }
+  } else {
+    if (!isNaN(value)) {
+      value += 'Gi'
+      commit('wizard/model$update', {
+        path: `/resources/${path}`,
+        value: value,
+        force: true,
+      })
+    }
+  }
+}
 return {
+  handleUnit,
   setMetadata,
   isKubedb,
   getDbDetails,
--- charts/kubedbcom-singlestore-editor/ui/functions.js
+++ charts/kubedbcom-singlestore-editor/ui/functions.js
@@ -2750,7 +2750,42 @@ function setApplyToIfReady() {
   return 'IfReady'
 }
 
+function handleUnit({ commit, model, getValue }, path, type = 'bound') {
+  let value = getValue(model, `/resources/${path}`)
+  if (type === 'scalingRules') {
+    const updatedValue = []
+    value.forEach((ele) => {
+      let appliesUpto = ele['appliesUpto']
+      let threshold = ele['threshold']
+      if (appliesUpto && !isNaN(appliesUpto)) {
+        appliesUpto += 'Gi'
+      }
+      if (!isNaN(threshold)) {
+        threshold += 'pc'
+      }
+      updatedValue.push({ threshold, appliesUpto })
+    })
+    if (JSON.stringify(updatedValue) !== JSON.stringify(value)) {
+      commit('wizard/model$update', {
+        path: `/resources/${path}`,
+        value: updatedValue,
+        force: true,
+      })
+    }
+  } else {
+    if (!isNaN(value)) {
+      value += 'Gi'
+      commit('wizard/model$update', {
+        path: `/resources/${path}`,
+        value: value,
+        force: true,
+      })
+    }
+  }
+}
+
 return {
+  handleUnit,
   isConsole,
   getNamespaces,
   getDbs,
