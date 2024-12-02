0f4c02725dabec2a78ea20536bf503af408255c1
SayedTahsin <tahsin@appscode.com>
2024-12-02 10:55:29 +0600

    druid set-trigger
    Signed-off-by: SayedTahsin <tahsin@appscode.com>

--- charts/kubedbcom-druid-editor/ui/functions.js
+++ charts/kubedbcom-druid-editor/ui/functions.js
@@ -3017,7 +3017,9 @@ function setControlledResources({ commit }, type) {
   return list
 }
 
-function setTrigger() {
+function setTrigger({ model, getValue }, path) {
+  let value = getValue(model, `/resources/${path}`)
+  if (value) return value
   return 'On'
 }
 
