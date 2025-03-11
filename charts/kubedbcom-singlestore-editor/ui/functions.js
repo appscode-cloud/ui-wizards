ab43ea1b6f76323a9290473f153aae967748993e
SayedTahsin <tahsin@appscode.com>
2025-03-11 11:21:35 +0600

    schema path fix for singlestore monitoring
    Signed-off-by: SayedTahsin <tahsin@appscode.com>

--- charts/kubedbcom-singlestore-editor/ui/functions.js
+++ charts/kubedbcom-singlestore-editor/ui/functions.js
@@ -2946,7 +2946,27 @@ function setMetadata({ storeGet, mode, commit }) {
   }
 }
 
+function getOpsRequestUrl({ storeGet, model, getValue, mode }, reqType) {
+  const cluster = storeGet('/route/params/cluster')
+  const domain = storeGet('/domain') || ''
+  const owner = storeGet('/route/params/user')
+  const dbname = getValue(model, '/metadata/release/name')
+  const group = getValue(model, '/metadata/resource/group')
+  const kind = getValue(model, '/metadata/resource/kind')
+  const namespace = getValue(model, '/metadata/release/namespace')
+  const resource = getValue(model, '/metadata/resource/name')
+  const version = getValue(model, '/metadata/resource/version')
+  const routeRootPath = storeGet('/route/path')
+  const pathPrefix = `${domain}/db${routeRootPath}`
+
+  if (mode === 'standalone-step')
+    return `${pathPrefix}?namespace=${namespace}&applyAction=create-opsrequest-${reqType.toLowerCase()}`
+  else
+    return `${domain}/console/${owner}/kubernetes/${cluster}/ops.kubedb.com/v1alpha1/mariadbopsrequests/create?name=${dbname}&namespace=${namespace}&group=${group}&version=${version}&resource=${resource}&kind=${kind}&page=operations&requestType=VerticalScaling`
+}
+
 return {
+  getOpsRequestUrl,
   setMetadata,
   handleUnit,
   isConsole,
