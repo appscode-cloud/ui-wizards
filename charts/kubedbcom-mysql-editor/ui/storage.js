d7338685fc89d67c6093b739a5db95a1e3c59a88
sourav-roy <1902036.souravroy@gmail.com>
2025-10-23 12:56:03 +0600

    false commit
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>

---
+++ charts/kubedbcom-mysql-editor/ui/storage.js
@@ -0,0 +1,33 @@
+function handleUnit(path, type = 'bound') {
+  let value = getValue(model, `/resources/${path}`)
+  if (type === 'scalingRules') {
+    const updatedValue = []
+    value?.forEach((ele) => {
+      let appliesUpto = ele['appliesUpto']
+      let threshold = ele['threshold']
+      if (appliesUpto && !isNaN(appliesUpto)) {
+        appliesUpto += 'Gi'
+      }
+      if (!isNaN(threshold)) {
+        threshold += 'pc'
+      }
+      updatedValue.push({ threshold, appliesUpto })
+    })
+    if (JSON.stringify(updatedValue) !== JSON.stringify(value)) {
+      commit('wizard/model$update', {
+        path: `/resources/${path}`,
+        value: updatedValue,
+        force: true,
+      })
+    }
+  } else {
+    if (!isNaN(value)) {
+      value += 'Gi'
+      commit('wizard/model$update', {
+        path: `/resources/${path}`,
+        value: value,
+        force: true,
+      })
+    }
+  }
+}
