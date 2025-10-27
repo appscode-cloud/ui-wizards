8f9ae004c5b4e940daa5dbcfd5065d244b0f5d04
sourav-roy <1902036.souravroy@gmail.com>
2025-10-27 12:05:44 +0600

    druid done
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>

---
+++ charts/kubedbcom-druid-editor/ui/storage.js
@@ -0,0 +1,62 @@
+  function isKubedb() {
+    console.log('isKubedb')
+    return !!storeGet('/route/params/actions')
+  }
+
+  function showOpsRequestOptions() {
+    console.log('showOpsRequestOptions')
+    if (isKubedb() === true) return true
+    return (
+      !!getValue(
+        model,
+        '/resources/autoscalingKubedbComDruidAutoscaler/spec/databaseRef/name',
+      ) && !!getValue(discriminator, '/autoscalingType')
+    )
+  }
+
+  function setTrigger(path) {
+    console.log('setTrigger')
+    let value = getValue(model, `/resources/${path}`)
+    if (value) return value
+    return 'On'
+  }
+
+  function setApplyToIfReady() {
+    console.log('setApplyToIfReady')
+    return 'IfReady'
+  }
+
+  function handleUnit(path, type = 'bound') {
+    console.log('handleUnit')
+    let value = getValue(model, `/resources/${path}`)
+    if (type === 'scalingRules') {
+      const updatedValue = []
+      value?.forEach((ele) => {
+        let appliesUpto = ele['appliesUpto']
+        let threshold = ele['threshold']
+        if (appliesUpto && !isNaN(appliesUpto)) {
+          appliesUpto += 'Gi'
+        }
+        if (!isNaN(threshold)) {
+          threshold += 'pc'
+        }
+        updatedValue.push({ threshold, appliesUpto })
+      })
+      if (JSON.stringify(updatedValue) !== JSON.stringify(value)) {
+        commit('wizard/model$update', {
+          path: `/resources/${path}`,
+          value: updatedValue,
+          force: true,
+        })
+      }
+    } else {
+      if (!isNaN(value)) {
+        value += 'Gi'
+        commit('wizard/model$update', {
+          path: `/resources/${path}`,
+          value: value,
+          force: true,
+        })
+      }
+    }
+  }
