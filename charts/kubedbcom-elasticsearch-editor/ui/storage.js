b1a3e029b3c7f6d8bbdb22e9eaaf0804388503ec
Sourav Roy <1902036souravroy@gmail.com>
2025-10-29 12:25:42 +0600

    Storage-autoscaller (#877)
    * yaml created via claude
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * yaml created via claude
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * yaml created via claud
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * file edited
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * false commit
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * false commit
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * false commit
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * false commit
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * pg done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * pg done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * mysql running
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * mysql done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * mariadb running
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * mariadb done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * mysql done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * kafka done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * druid running
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * druid done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * redis running
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * redis running
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * redis done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * singleStore done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * RabbitMq done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * perconaxtradb done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * mssqlserver done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * elasticsearch done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * elasticsearch done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * clickhouse done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * postgres done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * postgres done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * mysql done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * mariadb done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * fix kf maria mysql pg (#878)
    
    Signed-off-by: Samiul <samiul@appscode.com>
    Signed-off-by: Md. Samiul Haque <samiul@appscode.com>
    
    ---------
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    Signed-off-by: Samiul <samiul@appscode.com>
    Signed-off-by: Md. Samiul Haque <samiul@appscode.com>
    Co-authored-by: sourav-roy <1902036.souravroy@gmail.com>
    Co-authored-by: Md. Samiul Haque <samiul@appscode.com>

---
+++ charts/kubedbcom-elasticsearch-editor/ui/storage.js
@@ -0,0 +1,58 @@
+function isKubedb() {
+    return !!storeGet('/route/params/actions')
+  }
+
+  function showOpsRequestOptions() {
+    if (isKubedb() === true) return true
+
+    return (
+      !!getValue(
+        model,
+        '/resources/autoscalingKubedbComElasticsearchAutoscaler/spec/databaseRef/name',
+      ) && !!getValue(discriminator, '/autoscalingType')
+    )
+  }
+
+  function setTrigger(path) {
+    let value = getValue(model, `/resources/${path}`)
+    if (value) return value
+    return 'On'
+  }
+
+  function setApplyToIfReady() {
+    return 'IfReady'
+  }
+
+  function handleUnit(path, type = 'bound') {
+    let value = getValue(model, `/resources/${path}`)
+    if (type === 'scalingRules') {
+      const updatedValue = []
+      value?.forEach((ele) => {
+        let appliesUpto = ele['appliesUpto']
+        let threshold = ele['threshold']
+        if (appliesUpto && !isNaN(appliesUpto)) {
+          appliesUpto += 'Gi'
+        }
+        if (!isNaN(threshold)) {
+          threshold += 'pc'
+        }
+        updatedValue.push({ threshold, appliesUpto })
+      })
+      if (JSON.stringify(updatedValue) !== JSON.stringify(value)) {
+        commit('wizard/model$update', {
+          path: `/resources/${path}`,
+          value: updatedValue,
+          force: true,
+        })
+      }
+    } else {
+      if (!isNaN(value)) {
+        value += 'Gi'
+        commit('wizard/model$update', {
+          path: `/resources/${path}`,
+          value: value,
+          force: true,
+        })
+      }
+    }
+  }
