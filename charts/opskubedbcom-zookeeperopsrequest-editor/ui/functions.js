cb3ab83fb0f83f8213598ab341e99b968a1e7bce
Shofiqur Rahman <shofiqurahman15@gmail.com>
2025-11-27 10:52:30 +0600

    fix initial values for machine profile in all dbs (#902)
    Signed-off-by: shofiq <shofiq@appscode.com>
    Signed-off-by: samiul <samiul@appscode.com>
    Signed-off-by: Samiul <samiul@appscode.com>

--- charts/opskubedbcom-zookeeperopsrequest-editor/ui/functions.js
+++ charts/opskubedbcom-zookeeperopsrequest-editor/ui/functions.js
@@ -966,7 +966,8 @@ export const useFunc = (model) => {
   function getMachines() {
     const presets = storeGet('/kubedbuiPresets') || {}
     const dbDetails = getValue(discriminator, '/dbDetails')
-    const limits = dbDetails?.spec?.podTemplate?.spec?.resources?.limits || {}
+    // const limits = dbDetails?.spec?.podTemplate?.spec?.resources?.limits || {}
+    const limits = dbDetails?.spec?.podTemplate?.spec?.containers?.[0]?.resources?.requests
     const avlMachines = presets.admin?.machineProfiles?.available || []
     let arr = []
     if (avlMachines.length) {
@@ -1014,7 +1015,8 @@ export const useFunc = (model) => {
 
   function setMachine() {
     const dbDetails = getValue(discriminator, '/dbDetails')
-    const limits = dbDetails?.spec?.podTemplate?.spec?.resources?.limits || {}
+    // const limits = dbDetails?.spec?.podTemplate?.spec?.resources?.limits || {}
+    const limits = dbDetails?.spec?.podTemplate?.spec?.containers?.[0]?.resources?.requests
     const annotations = dbDetails?.metadata?.annotations || {}
     const machine = annotations['kubernetes.io/instance-type'] || 'custom'
 
