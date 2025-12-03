1d4946a75af7172c7da571289f0e3af1deac71d8
sourav-roy <1902036.souravroy@gmail.com>
2025-12-03 16:36:30 +0600

    mariadb trigger field
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>

--- charts/kubedbcom-mariadb-editor/ui/functions.js
+++ charts/kubedbcom-mariadb-editor/ui/functions.js
@@ -922,8 +922,21 @@ export const useFunc = (model) => {
 
   function setTrigger(path) {
     let value = getValue(model, `/resources/${path}`)
-    if (value) return value
-    return 'On'
+    console.log('setTrigger', value, path)
+
+    return value === 'On'
+  }
+
+  function onTriggerChange(type) {
+    const trigger = getValue(discriminator, `/${type}/trigger`)
+    console.log('trigger', trigger, type)
+    const commitPath = `/resources/autoscalingKubedbComMariaDBAutoscaler/spec/${type}/trigger`
+
+    commit('wizard/model$update', {
+      path: commitPath,
+      value: trigger ? 'On' : 'Off',
+      force: true,
+    })
   }
 
   function hasAnnotations() {
@@ -1413,6 +1426,7 @@ export const useFunc = (model) => {
     initMetadata,
     fetchTopologyMachines,
     setTrigger,
+    onTriggerChange,
     hasAnnotations,
     setAllowedMachine,
     getMachines,
