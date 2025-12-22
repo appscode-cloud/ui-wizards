c598f93b102e91ff8bb9079dd7a580fa49b4518b
Sourav Roy <1902036souravroy@gmail.com>
2025-12-22 10:05:45 +0600

    implemented new design in create-db ui for all databases
    * create forms
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * create form editing
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * mongodb create form ready
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * making horizontal done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * mongo done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * mysql done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * postgres running
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * function changding fixed
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * values not changind in shard mode
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * elasticsearch done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * kafka done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * mysql done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * pgbouncer done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * zookeeper done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * zookeeper don
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * percona done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * solr done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * Singlestore done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * redis done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * rabbitmq done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * proxysql
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * mssql done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * mariadb done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * druid done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * hazlecast done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * ignite done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * Cassandra done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * oracle done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * memcached done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * done
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * postgres issue fixed
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    
    * update and add missing description
    
    Signed-off-by: topoti <topoti@appscode.com>
    
    ---------
    
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>
    Signed-off-by: topoti <topoti@appscode.com>
    Co-authored-by: sourav-roy <1902036.souravroy@gmail.com>
    Co-authored-by: topoti <topoti@appscode.com>
    Signed-off-by: samiul <samiul@appscode.com>
    Signed-off-by: Samiul <samiul@appscode.com>
    Signed-off-by: Nihal Azmain <nihal@appscode.com>

--- charts/kubedbcom-mongodb-editor-options/ui/functions.js
+++ charts/kubedbcom-mongodb-editor-options/ui/functions.js
@@ -468,11 +468,10 @@ export const useFunc = (model) => {
     // watchDependency(`model#${fullpath}`)
     return modelPathValue !== 'custom' && !!modelPathValue
   }
-
+  let array = []
   function getMachineListForOptions() {
     const machinesFromPreset = getValue(model, '/spec/admin/machineProfiles/machines')
     const available = getValue(model, '/spec/admin/machineProfiles/available')
-    let array = []
 
     if (available.length) {
       array = available.map((machine) => {
@@ -533,7 +532,6 @@ export const useFunc = (model) => {
         memory = machines[selectedMachine].resources.limits.memory
       }
     }
-
     if (resource === 'memory') {
       commit('wizard/model$update', {
         path: reqCommitPath,
@@ -569,11 +567,51 @@ export const useFunc = (model) => {
     const commitPath = type
       ? `/spec/${type}/podResources/resources/limits/${resource}`
       : `/spec/podResources/resources/limits/${resource}`
+
+    const fullpath = `/spec/podResources/machine`
+    const modelPathValue = getValue(model, fullpath)
+
     commit('wizard/model$update', {
       path: commitPath,
       value: val,
       force: true,
     })
+
+    if (modelPathValue === 'custom') {
+      return
+    }
+    let commitCpuMemory, ModelPathValue
+    if (resource && type) {
+      const fullPath = `/spec/${type}/podResources/machine`
+      ModelPathValue = getValue(model, fullPath)
+      commitCpuMemory = `spec/${type}/podResources/resources/requests/${resource}`
+    } else {
+      const fullPath = `/spec/podResources/machine`
+      ModelPathValue = getValue(model, fullPath)
+      commitCpuMemory = `spec/podResources/resources/requests/${resource}`
+    }
+    let cpuMemoryValue
+    array.forEach((item) => {
+      if (item.value === ModelPathValue) {
+        // Parse subText like "CPU: 2, Memory: 2Gi"
+        const subText = item.subText || ''
+        if (resource === 'cpu') {
+          // Extract CPU value
+          const cpuMatch = subText.match(/CPU:\s*([^,]+)/)
+          cpuMemoryValue = cpuMatch ? cpuMatch[1].trim() : ''
+        } else if (resource === 'memory') {
+          // Extract Memory value
+          const memoryMatch = subText.match(/Memory:\s*(.+)/)
+          cpuMemoryValue = memoryMatch ? memoryMatch[1].trim() : ''
+        }
+      }
+    })
+    commit('wizard/model$update', {
+      path: commitCpuMemory,
+      value: cpuMemoryValue,
+      force: true,
+    })
+    return cpuMemoryValue
   }
 
   function setMachineToCustom(type) {
