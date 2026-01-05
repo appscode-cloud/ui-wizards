2b104f8bf82fad63df6823fb622578ac7de2e0cd
sourav-roy <1902036.souravroy@gmail.com>
2026-01-05 17:19:41 +0600

    New config secret done
    Signed-off-by: sourav-roy <1902036.souravroy@gmail.com>

--- charts/opskubedbcom-mongodbopsrequest-editor/ui/functions.js
+++ charts/opskubedbcom-mongodbopsrequest-editor/ui/functions.js
@@ -933,6 +933,7 @@ export const useFunc = (model) => {
   }
 
   let ConfigurationsData = []
+
   async function getConfigSecretsforAppyConfig() {
     const owner = storeGet('/route/params/user')
     const cluster = storeGet('/route/params/cluster')
