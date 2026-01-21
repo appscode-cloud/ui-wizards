e2d633999bad086f70c0098fb88ba7ed75810605
shofiq <shofiq@appscode.com>
2026-01-21 16:29:01 +0600

    fix paused init value in backup configuration
    Signed-off-by: shofiq <shofiq@appscode.com>

--- charts/kubedbcom-singlestore-editor/ui/functions.js
+++ charts/kubedbcom-singlestore-editor/ui/functions.js
@@ -488,6 +488,16 @@ export const useFunc = (model) => {
     return !!configName && contex === 'Modify'
   }
 
+  function setPausedValue() {
+    const backupConfig = storeGet('backup/backupConfigurations') || []
+    const selectedConfigName = getValue(discriminator, '/config')
+    const namespace = storeGet('/route/query/namespace')
+    const selectedConfig = backupConfig.find(
+      (item) => item.metadata.name === selectedConfigName && item.metadata.namespace === namespace,
+    )
+    return !!selectedConfig?.spec?.paused
+  }
+
   function showConfigList() {
     // watchDependency('discriminator#/backupConfigContext')
     const contex = getValue(discriminator, '/backupConfigContext')
@@ -1537,6 +1547,7 @@ export const useFunc = (model) => {
     showScheduleBackup,
     getDefaultSchedule,
     onInputChangeSchedule,
+    setPausedValue,
 
     isConsole,
     isKubedb,
