25139ad0f16d90712198c28c7be110649edbcdd3
Sourav Roy <1902036souravroy@gmail.com>
2026-01-21 17:27:53 +0600

    fix match null issue (#950)
    Signed-off-by: Samiul <samiul@appscode.com>
    Signed-off-by: Nihal Azmain <nihal@appscode.com>

--- charts/opskubedbcom-druidopsrequest-editor/ui/functions.js
+++ charts/opskubedbcom-druidopsrequest-editor/ui/functions.js
@@ -895,8 +895,13 @@ export const useFunc = (model) => {
     machinesFromPreset = storeGet('/kubedbuiPresets')?.admin?.machineProfiles?.machines || []
 
     const machinePresets = machinesFromPreset.find((item) => item.id === machine)
-    if (machinePresets) return machine
-    else return { machine: 'custom', cpu: limits.cpu, memory: limits.memory }
+    if (machinePresets) {
+      return {
+        machine: machine,
+        cpu: machinePresets.limits.cpu,
+        memory: machinePresets.limits.memory,
+      }
+    } else return { machine: 'custom', cpu: limits.cpu, memory: limits.memory }
   }
 
   function onMachineChange(type, path) {
