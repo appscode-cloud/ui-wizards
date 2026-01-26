9461330c475f865f955a512aca409df9900dc1c8
Md. Samiul Haque <samiul@appscode.com>
2026-01-26 15:43:13 +0600

    exporter input fields in vertical scaling (#956)
    Signed-off-by: Samiul <samiul@appscode.com>

--- charts/opskubedbcom-pgpoolopsrequest-editor/ui/functions.js
+++ charts/opskubedbcom-pgpoolopsrequest-editor/ui/functions.js
@@ -1296,7 +1296,34 @@ export const useFunc = (model) => {
     return !!(model && model.alias)
   }
 
+  function setExporter(type) {
+    let path = `/dbDetails/spec/monitor/prometheus/exporter/resources/limits/${type}`
+    const limitVal = getValue(discriminator, path)
+
+    if (!limitVal) {
+      path = `/dbDetails/spec/monitor/prometheus/exporter/resources/requests/${type}`
+      const reqVal = getValue(discriminator, path)
+
+      if (reqVal) return reqVal
+    }
+    return limitVal
+  }
+
+  function onExporterResourceChange(type) {
+    const commitPath = `/spec/verticalScaling/exporter/resources/requests/${type}`
+    const valPath = `/spec/verticalScaling/exporter/resources/limits/${type}`
+    const val = getValue(model, valPath)
+    if (val)
+      commit('wizard/model$update', {
+        path: commitPath,
+        value: val,
+        force: true,
+      })
+  }
+
   return {
+    setExporter,
+    onExporterResourceChange,
     fetchAliasOptions,
     validateNewCertificates,
     disableAlias,
