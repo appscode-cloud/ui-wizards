c318f8d45764f90e94a02cf66c87e69da1d1dd99
Shofiqur Rahman <shofiqurahman15@gmail.com>
2026-01-27 18:22:24 +0600

    convert env block to array object & ops-request url fix
    * add endpoints required & fix create ops-request url
    
    Signed-off-by: shofiq <shofiq@appscode.com>
    
    * fix mongos env object to make values & names as required
    
    Signed-off-by: shofiq <shofiq@appscode.com>
    
    * fix tmep error in onEnvArrayChange of monitoring
    
    Signed-off-by: shofiq <shofiq@appscode.com>
    
    * add array env in monitoring to all supported dbs
    
    Signed-off-by: shofiq <shofiq@appscode.com>
    
    * fix pg pool config key in dbInfo api payload
    
    Signed-off-by: shofiq <shofiq@appscode.com>
    
    * fix button class in memcached monitoring endpoints
    
    Signed-off-by: shofiq <shofiq@appscode.com>
    
    ---------
    
    Signed-off-by: shofiq <shofiq@appscode.com>

--- charts/kubedbcom-kafka-editor/ui/functions.js
+++ charts/kubedbcom-kafka-editor/ui/functions.js
@@ -16,6 +16,7 @@ export const useFunc = (model) => {
   setDiscriminatorValue('/enableMonitoring', false)
   setDiscriminatorValue('/customizeExporter', true)
   setDiscriminatorValue('/valueFromType', 'input')
+  setDiscriminatorValue('/env', [])
 
   // Autoscaler Discriminators
   setDiscriminatorValue('/dbDetails', false)
@@ -610,8 +611,7 @@ export const useFunc = (model) => {
     const routeRootPath = storeGet('/route/path')
     const pathPrefix = `${domain}/db${routeRootPath}`
     const pathSplit = pathPrefix.split('/').slice(0, -1).join('/')
-    const pathConstructedForKubedb =
-      pathSplit + `/create-opsrequest-${reqType.toLowerCase()}?namespace=${namespace}`
+    const pathConstructedForKubedb = pathSplit + `/${reqType.toLowerCase()}?namespace=${namespace}`
 
     const isKube = !!storeGet('/route/params/actions')
 
@@ -710,11 +710,11 @@ export const useFunc = (model) => {
     }
   }
 
-  function isEqualToValueFromType(value) {
-    //watchDependency('discriminator#/valueFromType')
-    const valueFrom = getValue(discriminator, '/valueFromType')
-    return valueFrom === value
-  }
+  // function isEqualToValueFromType(value) {
+  //   //watchDependency('discriminator#/valueFromType')
+  //   const valueFrom = getValue(discriminator, '/valueFromType')
+  //   return valueFrom === value
+  // }
 
   async function resourceNames(group, version, resource) {
     const namespace = getValue(model, '/metadata/release/namespace')
@@ -743,14 +743,14 @@ export const useFunc = (model) => {
     })
   }
 
-  async function getConfigMapKeys() {
+  async function getConfigMapKeys(index) {
     const owner = storeGet('/route/params/user')
     const cluster = storeGet('/route/params/cluster')
     // const namespace = getValue(reusableElementCtx, '/dataContext/namespace') // not supported
     const namespace = getValue(model, '/metadata/release/namespace')
     const configMapName = getValue(
       model,
-      '/resources/kubedbComKafka/spec/monitor/prometheus/exporter/env/items/valueFrom/configMapKeyRef/name',
+      `/resources/kubedbComKafka/spec/monitor/prometheus/exporter/env/${index}/valueFrom/configMapKeyRef/name`,
     )
 
     // watchDependency('data#/namespace')
@@ -813,14 +813,14 @@ export const useFunc = (model) => {
     }
   }
 
-  async function getSecretKeys() {
+  async function getSecretKeys(index) {
     const owner = storeGet('/route/params/user')
     const cluster = storeGet('/route/params/cluster')
     // const namespace = getValue(reusableElementCtx, '/dataContext/namespace') // not supported
     const namespace = getValue(model, '/metadata/release/namespace')
     const secretName = getValue(
       model,
-      '/resources/kubedbComKafka/spec/monitor/prometheus/exporter/env/items/valueFrom/secretKeyRef/name',
+      `/resources/kubedbComKafka/spec/monitor/prometheus/exporter/env/${index}/valueFrom/secretKeyRef/name`,
     )
 
     // watchDependency('data#/namespace')
@@ -935,6 +935,62 @@ export const useFunc = (model) => {
     return value
   }
 
+  function onEnvArrayChange() {
+    const env = getValue(discriminator, '/env') || []
+    let ret = {}
+    // filter out temp values
+    const filteredEnv = env?.map((item) => {
+      const { temp, ...rest } = item
+      if (temp?.valueFromType === 'input') {
+        const { name, value } = rest
+        ret = { name, value }
+      } else if (temp?.valueFromType === 'configMap') {
+        const { name } = rest
+        const { configMapKeyRef } = rest?.valueFrom || {}
+        ret = { name, valueFrom: { configMapKeyRef } }
+      } else if (temp?.valueFromType === 'secret') {
+        const { name } = rest
+        const { secretKeyRef } = rest?.valueFrom || {}
+        ret = { name, valueFrom: { secretKeyRef } }
+      }
+      return ret
+    })
+
+    if (filteredEnv.length)
+      commit('wizard/model$update', {
+        path: '/resources/kubedbComKafka/spec/monitor/prometheus/exporter/env',
+        value: filteredEnv,
+        force: true,
+      })
+  }
+
+  function initEnvArray() {
+    const env = getValue(model, '/resources/kubedbComKafka/spec/monitor/prometheus/exporter/env')
+
+    return env || []
+  }
+
+  function isEqualToTemp(value, index) {
+    //watchDependency('discriminator#/valueFromType')
+    const valueFrom = getValue(discriminator, `/env/${index}/temp/valueFromType`)
+    return valueFrom === value
+  }
+
+  function initMonitoring() {
+    const env =
+      getValue(model, '/resources/kubedbComKafka/spec/monitor/prometheus/exporter/env') || []
+    setDiscriminatorValue('/env', env)
+    let tempEnv = []
+    env.forEach((item) => {
+      let radio = ''
+      if (item.value) radio = 'input'
+      else if (item.valueFrom && item.valueFrom.configMapKeyRef) radio = 'configMap'
+      else if (item.valueFrom && item.valueFrom.secretKeyRef) radio = 'secret'
+      tempEnv.push({ ...item, temp: { valueFromType: radio } })
+    })
+    setDiscriminatorValue('/env', tempEnv)
+  }
+
   return {
     isKubedb,
     isConsole,
@@ -972,7 +1028,6 @@ export const useFunc = (model) => {
     onLabelChange,
     setValueFrom,
     onValueFromChange,
-    isEqualToValueFromType,
     resourceNames,
     getConfigMapKeys,
     getSecrets,
@@ -980,6 +1035,10 @@ export const useFunc = (model) => {
     isConfigMapTypeValueFrom,
     isSecretTypeValueFrom,
     getNamespacedResourceList,
+    onEnvArrayChange,
+    initEnvArray,
+    isEqualToTemp,
+    initMonitoring,
 
     isBindingAlreadyOn,
     addOrRemoveBinding,
