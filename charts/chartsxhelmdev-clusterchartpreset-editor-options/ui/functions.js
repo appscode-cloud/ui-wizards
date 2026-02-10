e5e6a166b53795e68799bad7cc2b55057b5bd23a
shofiq <shofiq@appscode.com>
2026-02-10 11:26:18 +0600

    fix stash presets loader & multiselect watcher in configDatabase
    Signed-off-by: shofiq <shofiq@appscode.com>

--- charts/chartsxhelmdev-clusterchartpreset-editor-options/ui/functions.js
+++ charts/chartsxhelmdev-clusterchartpreset-editor-options/ui/functions.js
@@ -514,20 +514,12 @@ export const useFunc = (model) => {
 
   function clearDefaultVersion(db) {
     const data = getValue(model, `/spec/admin/databases/${db}/versions/available`)
+    if (data?.includes('Remove all')) return []
     if (data?.includes('Select all')) {
-      versions = getValue(discriminator, `allDbVersions/${db}Version`)
-      commit('wizard/model$update', {
-        path: `/spec/admin/databases/${db}/versions/available`,
-        value: versions,
-        force: true,
-      })
-    }
-    if (data?.includes('Remove all')) {
-      commit('wizard/model$update', {
-        path: `/spec/admin/databases/${db}/versions/available`,
-        value: [],
-        force: true,
-      })
+      const versions = getValue(discriminator, `allDbVersions/${db}Version`) ?? []
+      return versions
+        .filter((v) => v !== 'Select all' && v !== 'Remove all')
+        .map((v) => ({ text: v, value: v }))
     }
   }
 
@@ -972,6 +964,80 @@ export const useFunc = (model) => {
     })
   }
 
+  async function getNamespaces() {
+    const params = storeGet('/route/params')
+    const { user, cluster, group, version, resource } = params
+    try {
+      const resp = await axios.post(
+        `/clusters/${user}/${cluster}/proxy/identity.k8s.appscode.com/v1alpha1/selfsubjectnamespaceaccessreviews`,
+        {
+          apiVersion: 'identity.k8s.appscode.com/v1alpha1',
+          kind: 'SelfSubjectNamespaceAccessReview',
+          spec: {
+            resourceAttributes: [
+              {
+                verb: 'create',
+                group: group,
+                version: version,
+                resource: resource,
+              },
+            ],
+          },
+        },
+      )
+      if (resp.data?.status?.projects) {
+        const projects = resp.data?.status?.projects
+        let projectsNamespace = []
+        projectsNamespace = Object.keys(projects).map((project) => ({
+          project: project,
+          namespaces: projects[project].map((namespace) => ({
+            text: namespace,
+            value: namespace,
+          })),
+        }))
+        return projectsNamespace
+      } else {
+        return resp.data?.status?.namespaces || []
+      }
+    } catch (e) {
+      console.log(e)
+    }
+    return []
+  }
+
+  function isRancherManaged() {
+    const managers = storeGet('/cluster/clusterDefinition/result/clusterManagers')
+    const found = managers.find((item) => item === 'Rancher')
+    return !!found
+  }
+
+  async function fetchNames(type) {
+    // watchDependency(`model#/${type}/namespace`)
+    const username = storeGet('/route/params/user')
+    const clusterName = storeGet('/route/params/cluster')
+    const namespace = getValue(model, `/spec/backup/kubestash/${type}/namespace`)
+    const suffix =
+      type === 'encryptionSecret'
+        ? 'secrets'
+        : type === 'retentionPolicy'
+          ? 'retentionpolicies'
+          : 'backupstorages'
+    const core = suffix === 'secrets' ? 'core' : 'storage.kubestash.com'
+    const version = suffix === 'secrets' ? 'v1' : 'v1alpha1'
+    const url = `/clusters/${username}/${clusterName}/proxy/${core}/${version}/namespaces/${namespace}/${suffix}`
+    try {
+      if (namespace) {
+        const resp = await axios.get(url)
+        let names = resp?.data?.items
+        names = names.map((ele) => ele?.metadata?.name)
+        return names
+      }
+    } catch (e) {
+      console.log(e)
+    }
+    return []
+  }
+
   return {
     preSelectClusterIssuer,
     isRancherManaged,
@@ -1011,5 +1077,8 @@ export const useFunc = (model) => {
     setCustomAvlMachine,
     onMachineProfileChange,
     setMachineProfiles,
+    isRancherManaged,
+    getNamespaces,
+    fetchNames,
   }
 }
