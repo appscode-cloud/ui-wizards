14bce14c8710c273fc36f031548da2a10b7691ef
Md. Samiul Haque <samiul@appscode.com>
2026-07-15 10:58:08 +0600

    fix backup default values (#1086)
    Signed-off-by: Samiul <samiul@appscode.com>

--- charts/kubedbcom-oracle-editor/ui/functions.js
+++ charts/kubedbcom-oracle-editor/ui/functions.js
@@ -160,7 +160,106 @@ export const useFunc = (model) => {
   let initialDbMetadata = {}
   let namespaceList = []
   let backupConfigurationsFromStore = {}
-  let valuesFromWizard = {}
+  let valuesFromWizard = {
+    apiVersion: 'core.kubestash.com/v1alpha1',
+    kind: 'BackupConfiguration',
+    metadata: {
+      name: 'oracle',
+      namespace: 'demo',
+    },
+    spec: {
+      backends: [
+        {
+          name: 'oracle-backend',
+          retentionPolicy: {
+            name: 'oracle-retention-policy',
+            namespace: 'demo',
+          },
+          storageRef: {
+            name: 'oracle-storage',
+            namespace: 'demo',
+          },
+        },
+      ],
+      sessions: [
+        {
+          addon: {
+            jobTemplate: {
+              spec: {
+                containerSecurityContext: {
+                  allowPrivilegeEscalation: false,
+                  capabilities: {
+                    drop: ['ALL'],
+                  },
+                  runAsGroup: 0,
+                  runAsNonRoot: true,
+                  runAsUser: 999,
+                  seccompProfile: {
+                    type: 'RuntimeDefault',
+                  },
+                },
+                nodeSelector: {
+                  'kubernetes.io/os': 'linux',
+                },
+              },
+            },
+            name: 'oracle-addon',
+            tasks: [
+              {
+                name: 'logical-backup',
+              },
+            ],
+          },
+          name: 'oracle-frequent-backup',
+          repositories: [
+            {
+              backend: 'oracle-backend',
+              directory: '/oracle-repo',
+              encryptionSecret: {
+                name: 'oracle-encryption-secret',
+                namespace: 'demo',
+              },
+              name: 'oracle-repo',
+            },
+          ],
+          scheduler: {
+            failedJobsHistoryLimit: 4,
+            jobTemplate: {
+              backoffLimit: 2,
+              template: {
+                spec: {
+                  containerSecurityContext: {
+                    allowPrivilegeEscalation: false,
+                    capabilities: {
+                      drop: ['ALL'],
+                    },
+                    runAsGroup: 0,
+                    runAsNonRoot: true,
+                    runAsUser: 999,
+                    seccompProfile: {
+                      type: 'RuntimeDefault',
+                    },
+                  },
+                  nodeSelector: {
+                    'kubernetes.io/os': 'linux',
+                  },
+                },
+              },
+            },
+            schedule: '0 */2 * * *',
+            successfulJobsHistoryLimit: 2,
+          },
+          sessionHistoryLimit: 3,
+        },
+      ],
+      target: {
+        apiGroup: 'kubedb.com',
+        kind: 'Oracle',
+        name: 'oracle',
+        namespace: 'demo',
+      },
+    },
+  }
   let initialArchiver = {}
   let isArchiverAvailable = false
   let archiverObjectToCommit = {}
@@ -181,27 +280,6 @@ export const useFunc = (model) => {
     initialArchiver = dbResource.spec?.archiver ? objectCopy(dbResource.spec?.archiver) : undefined
 
     // get values.yaml to populate data when backup-config is being created
-    try {
-      const actionArray = storeGet('/resource/actions/result')
-      const editorDetails = actionArray[0]?.items[0]?.editor
-      const chartName = editorDetails?.name
-      const sourceApiGroup = editorDetails?.sourceRef?.apiGroup
-      const sourceKind = editorDetails?.sourceRef?.kind
-      const sourceNamespace = editorDetails?.sourceRef?.namespace
-      const sourceName = editorDetails?.sourceRef?.name
-      const chartVersion = editorDetails?.version
-
-      let url = `/clusters/${user}/${cluster}/helm/packageview/values?name=${chartName}&sourceApiGroup=${sourceApiGroup}&sourceKind=${sourceKind}&sourceNamespace=${sourceNamespace}&sourceName=${sourceName}&version=${chartVersion}&format=json`
-
-      if (spoke)
-        url = `/clusters/${user}/${spoke}/helm/packageview/values?name=${chartName}&sourceApiGroup=${sourceApiGroup}&sourceKind=${sourceKind}&sourceNamespace=${sourceNamespace}&sourceName=${sourceName}&version=${chartVersion}&format=json`
-
-      const resp = await axios.get(url)
-
-      valuesFromWizard = objectCopy(resp.data?.resources?.coreKubestashComBackupConfiguration) || {}
-    } catch (e) {
-      console.log(e)
-    }
 
     // check storageclass archiver annotation
     if (initialArchiver) {
