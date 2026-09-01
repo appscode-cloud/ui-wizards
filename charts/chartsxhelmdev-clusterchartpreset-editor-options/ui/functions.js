61e6440ef793d92a3cd6d582d2872ffa00e6d041
topoti <topoti@appscode.com>
2026-09-01 17:30:45 +0600

    Update machine profile memory for db.m.small
    Signed-off-by: topoti <topoti@appscode.com>

--- charts/chartsxhelmdev-clusterchartpreset-editor-options/ui/functions.js
+++ charts/chartsxhelmdev-clusterchartpreset-editor-options/ui/functions.js
@@ -94,11 +94,11 @@ export const useFunc = (model) => {
       resources: {
         requests: {
           cpu: '500m',
-          memory: '912680550',
+          memory: '0.85Gi',
         },
         limits: {
           cpu: '1',
-          memory: '1825361100',
+          memory: '1.7Gi',
         },
       },
     },
