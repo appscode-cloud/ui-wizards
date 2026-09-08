919680ae983cd716e1fc93fe93e49f3e5effd6f3
Arnob Kumar Saha <arnob@appscode.com>
2026-09-08 18:53:37 +0600

    Add KubeStash backup for milvus editor options (#1172)
    Signed-off-by: Arnob kumar saha <arnob@appscode.com>

--- charts/kubedbcom-milvus-editor-options/ui/functions.js
+++ charts/kubedbcom-milvus-editor-options/ui/functions.js
@@ -975,6 +975,21 @@ export const useFunc = (model) => {
     return !!agent
   }
 
+  function setBackup() {
+    const backup = getValue(model, '/spec/backup/tool')
+    const val = getValue(model, '/spec/admin/backup/enable/default')
+    return backup === 'KubeStash' && features.includes('backup') && val
+  }
+
+  function onBackupSwitch() {
+    const isBackupOn = getValue(discriminator, '/backup')
+    commit('wizard/model$update', {
+      path: '/spec/backup/tool',
+      value: isBackupOn ? 'KubeStash' : '',
+      force: true,
+    })
+  }
+
   function onAuthChange() {
     commit('wizard/model$update', {
       path: '/spec/authSecret/name',
@@ -1141,6 +1156,8 @@ export const useFunc = (model) => {
     showIssuer,
     showTlsDetails,
     setMonitoring,
+    setBackup,
+    onBackupSwitch,
     onAuthChange,
     isConfigDatabaseOn,
     clearConfiguration,
